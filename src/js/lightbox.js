c637a1e3e92b6a7fdff8fc874a8da4a38ec33c2b
Catouse <catouse@gmail.com>
2014-01-07 16:54:57 +0800

    * ajusted style of lightbox.

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -2,7 +2,11 @@
 {
     jQuery.fn.lightbox = function()
     {
-        $(this).click(function(){
+        var lightboxId = 0;
+        $("[data-toggle='lightbox']").each(function(){$(this).attr('data-id',lightboxId++)});
+
+        $(this).click(function()
+        {
             // need modal
             if (!$.fn.modal) throw new Error('modal requires for lightbox');
 
@@ -14,18 +18,51 @@
             if(!imgUrl) return false;
 
             // get caption
-            var caption = $e.attr('data-caption') || $e.find('img').attr('title') || $e.attr('title');
+            // var caption = $e.attr('data-caption') || $e.find('img').attr('title') || $e.attr('title');
 
             // get lightbox modal
             if($('#lightboxModal').size() == 0)
-                $('<div id="lightboxModal" class="modal fade modal-lightbox"><img class="modal-dialog" id="lightboxImg" src="#" alt="" data-dismiss="modal" /><div class="caption"></div></div>').appendTo('body');
+            {
+                $('<div id="lightboxModal" class="modal fade modal-lightbox"><div class="modal-dialog"><button class="close" data-dismiss="modal" aria-hidden="true"><i class="icon-remove"></i></button><button class="controller prev"><i class="icon icon-chevron-left"></i></button><button class="controller next"><i class="icon icon-chevron-right"></i></button><img id="lightboxImg" src="#" alt="" data-dismiss="modal" /><div class="caption"></div></div></div>').appendTo('body');
+                $('#lightboxModal .controller').click(function()
+                {
+                    var id = parseInt($('#lightboxModal').attr('data-id')) + ($(this).hasClass('prev') ? -1 : 1);
+                    var e = $('[data-toggle="lightbox"][data-id="' + id + '"]');
+                    if(e)
+                    {
+                        var url = e.attr('data-image') || e.attr('src') || e.attr('href') || e.find('img').attr('src');
+                        if(url)
+                        {
+                            $('#lightboxImg').attr('src', url);
+                            $('#lightboxModal').attr('data-id', id);
+
+                            $('#lightboxModal .controller').hide();
+                            if($('[data-toggle="lightbox"][data-id="' + (id - 1) + '"]').length > 0)
+                                $('#lightboxModal .prev').show(); 
+                            if($('[data-toggle="lightbox"][data-id="' + (id + 1) + '"]').length > 0)
+                                $('#lightboxModal .next').show(); 
+
+                            $('#lightboxModal .modal-dialog').width(e.attr('data-width') || 'auto').height(e.attr('data-height') || 'auto').css('margin-top', Math.max(0, ($(window).height() - e.attr('data-height'))/2));
+                        }
+                    }
+                });
+            }
+
+            var id = parseInt($e.attr('data-id'));
             var img = $('#lightboxImg');
             var lightboxModal = $('#lightboxModal');
 
+            // set current id
+            lightboxModal.find('.controller').hide();
+            lightboxModal.attr('data-id', id);
+            if($('[data-toggle="lightbox"][data-id="' + (id - 1) + '"]').length > 0) lightboxModal.find('.prev').show();
+            if($('[data-toggle="lightbox"][data-id="' + (id + 1) + '"]').length > 0) lightboxModal.find('.next').show();
+            lightboxModal.find('.modal-dialog').width($e.attr('data-width') || 'auto').height($e.attr('data-height') || 'auto').css('margin-top', Math.max(0, ($(window).height() - $e.attr('data-height'))/2));
+
             // update modal content
-            img.attr('alt', caption).attr('src', imgUrl);
-            if(caption && caption.length > 0) lightboxModal.addClass('with-caption').find('.caption').text(caption);
-            else lightboxModal.find('.caption').remove();
+            img.attr('src', imgUrl);
+            // if(caption && caption.length > 0) lightboxModal.addClass('with-caption').find('.caption').text(caption);
+            // else lightboxModal.find('.caption').remove();
 
             // show modal
             lightboxModal.modal();
