96d4b31738c70bde2148d0564ad419d9ea5650bf
Catouse <catouse@gmail.com>
2014-02-20 19:55:37 +0800

    + added data table.

--- src/js/zui.js
+++ src/js/zui.js
@@ -2111,6 +2111,70 @@ if (typeof jQuery === "undefined") { throw new Error("ZUI requires jQuery") }
     });
 }(jQuery,window,document,Math);
 
+/* DataTable */
++function($, window, document, Math)
+{
+    "use strict";
+
+    var DataTable = function(element, options)
+    {
+        this.$         = $(element);
+        this.options   = this.getOptions(options);
+
+        this.init();
+    };
+
+    DataTable.DEFAULTS = {};
+
+    DataTable.prototype.getOptions = function (options)
+    {
+        options = $.extend({}, DataTable.DEFAULTS, this.$.data(), options);
+        return options;
+    };
+
+    DataTable.prototype.init = function()
+    {
+        this.handleRowClickable();
+    }
+
+    DataTable.prototype.handleRowClickable = function()
+    {
+        var $dataTable = this.$;
+
+        this.$.find('tr[data-url] td:not(".actions")').click(function(event)
+        {
+            if($(event.target).is('a, .caret')) return;
+
+            var url = $(this).closest('tr').data('url');
+            if(url)
+            {
+                window.location = url;
+            }
+        });
+    }
+
+    $.fn.dataTable = function(option)
+    {
+        return this.each(function()
+        {
+            var $this   = $(this);
+            var data    = $this.data('zui.dataTable');
+            var options = typeof option == 'object' && option;
+
+            if (!data) $this.data('zui.dataTable', (data = new DataTable(this, options)));
+
+            if (typeof option == 'string') data[option]();
+        })
+    };
+
+    $.fn.dataTable.Constructor = DataTable;
+
+    $(function()
+    {
+        $('table.table-data').dataTable();
+    });
+}(jQuery,window,document,Math);
+
 
 /**
  * bootbox.js v4.1.0
