1cdfcd919ad0a1d59232782ac2d62b451f0b5418
Catouse <catouse@gmail.com>
2014-06-05 15:32:00 +0800

    * updated documents.

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -13,6 +13,20 @@ $(function()
       container: "body"
     })
 
-    $("[data-toggle=popover]")
-      .popover();
+    // popover demo
+    $("[data-toggle=popover]").popover();
+
+    // navbar collapse
+    $('.navbar-collapsed .nav > .nav-heading').click(function(event)
+    {
+        var $nav = $(this).closest('.nav');
+        if($nav.hasClass('collapsed'))
+        {
+            $nav.removeClass('collapsed').children('li:not(.nav-heading)').slideDown('fast');
+        }
+        else
+        {
+            $nav.children('li:not(.nav-heading)').slideUp('fast', function(){$nav.addClass('collapsed')});
+        }
+    });
 });
