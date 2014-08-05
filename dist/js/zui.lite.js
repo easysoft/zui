9bdd05fc683362349c701b58dec76c59969709a1
Catouse <catouse@gmail.com>
2014-08-05 15:15:27 +0800

    + added store.js.

--- dist/js/zui.lite.js
+++ dist/js/zui.lite.js
@@ -9,6 +9,154 @@
 
 if (typeof jQuery === "undefined") { throw new Error("ZUI requires jQuery") }
 
+/* Store */
++function(window)
+{
+    "use strict";
+
+    var lsName = 'localStorage';
+    var storage = window[lsName],
+        old = window['store'];
+
+    /* The Store object */
+    var Store = function()
+    {
+        this.slience = true;
+        this.disabled = (lsName in window) && window[lsName] && window[lsName]['setItem'];
+        this.storage = storage;
+    };
+
+    /* Check disabled status */
+    Store.prototype.check = function()
+    {
+        if(this.disabled)
+        {
+            if(!this.slience) throw new Error('Browser not support localStorage or disabled status been set true.');
+        }
+        return !this.disabled;
+    };
+
+    /* Get length */
+    Store.prototype.length = function()
+    {
+        if(this.check())
+        {
+            return storage.length;
+        }
+    };
+
+    /* Remove item with browser localstorage native method */
+    Store.prototype.removeItem = function(key)
+    {
+        storage.removeItem(key);
+    };
+
+    /* Remove item with browser localstorage native method, same as removeItem */
+    Store.prototype.remove = function(key)
+    {
+        this.removeItem(key);
+    };
+
+    /* Get item value with browser localstorage native method, and without deserialize */
+    Store.prototype.getItem = function(key)
+    {
+        return storage.getItem(key);
+    };
+
+    /* Get item value and deserialize it, if value is null and defaultValue been given then return defaultValue */
+    Store.prototype.get = function(key, defaultValue)
+    {
+        var val = this.deserialize(this.getItem(key));
+        return (defaultValue !== undefined && val === null) ? defaultValue : val;
+    };
+
+    /* Get item value by key and deserialize it */
+    Store.prototype.key = function(key)
+    {
+        return this.deserialize(storage.key(key));
+    };
+
+    /* Set item value with browser localstorage native method, and without serialize filter */
+    Store.prototype.setItem = function(key, val)
+    {
+        storage.setItem(key, val);
+    };
+
+    /* Set item value, serialize it if the given value is not an string */
+    Store.prototype.set = function(key, val)
+    {
+        if(val === undefined) return this.remove(key);
+        this.setItem(key, this.serialize(val));
+    };
+
+    /* Clear all items with browser localstorage native method */
+    Store.prototype.clear = function()
+    {
+        storage.clear(key);
+    };
+
+    /* Iterate all items with callback */
+    Store.prototype.forEach = function(callback)
+    {
+        for(var i = 0; i < storage.length; i++)
+        {
+            var key = storage.key(i);
+            callback(key, store.get(key));
+        }
+    };
+
+    /* Get all items and set value in an object. */
+    Store.prototype.getAll = function(callback)
+    {
+        var all = {};
+        this.forEach(function(key, val)
+        {
+            all[key] = val;
+        });
+
+        return all;
+    };
+
+    store.getAll = function() {
+      var ret = {}
+      store.forEach(function(key, val) {
+        ret[key] = val
+      })
+      return ret
+    }
+
+    /* Serialize value with JSON.stringify */
+    Store.prototype.serialize = function(value)
+    {
+        if(typeof value === 'string') return value;
+        return JSON.stringify(value);
+    };
+
+    /* Deserialize value, with JSON.parse if the given value is not a string */
+    Store.prototype.deserialize = function(value)
+    {
+        if(typeof value !== 'string') return undefined;
+        try
+        {
+            return JSON.parse(value);
+        }
+        catch(e)
+        {
+            return value || undefined;
+        }
+    };
+
+    var store = new Store();
+
+    window.store = store;
+
+    window.store.noConflict = function()
+    {
+        window.store = old;
+        return store;
+    }
+}(window);
+
 /**
  * Format string
  *  
@@ -450,25 +598,6 @@ String.prototype.isNum = function(s)
             return 1;
         }
     });
-
-    jQuery.fn.selectText = function()
-    {
-        var doc = document;
-        var element = this[0];
-        if (doc.body.createTextRange)
-        {
-            var range = document.body.createTextRange();
-            range.moveToElementText(element);
-            range.select();
-        } else if (window.getSelection)
-        {
-            var selection = window.getSelection();        
-            var range = document.createRange();
-            range.selectNodeContents(element);
-            selection.removeAllRanges();
-            selection.addRange(range);
-        }
-    };
 }(jQuery,window,document,Math);
 
 /* Device */
