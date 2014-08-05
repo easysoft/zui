5a34f9c376eafacff24817740f6cec2e57617622
Catouse <catouse@gmail.com>
2014-08-05 15:12:44 +0800

    * removed jquery extension 'selectText' from unities.js.

--- src/js/unities.js
+++ src/js/unities.js
@@ -35,23 +35,4 @@
             return 1;
         }
     });
-
-    jQuery.fn.selectText = function()
-    {
-        var doc = document;
-        var element = this[0];
-        if (doc.body.createTextRange)
-        {
-            var range = document.body.createTextRange();
-            range.moveToElementText(element);
-            range.select();
-        } else if (window.getSelection)
-        {
-            var selection = window.getSelection();        
-            var range = document.createRange();
-            range.selectNodeContents(element);
-            selection.removeAllRanges();
-            selection.addRange(range);
-        }
-    };
 }(jQuery,window,document,Math);
