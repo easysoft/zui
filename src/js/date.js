882b111df2501dce3ceb92511cf48fad94e52840
Catouse <catouse@gmail.com>
2014-08-14 09:07:39 +0800

    * added some useful function to date.

--- src/js/date.js
+++ src/js/date.js
@@ -29,3 +29,44 @@ Date.prototype.format = function(format)
     }
     return format;
 };
+
+Date.prototype.addDays = function(days)
+{
+    this.setTime(this.getTime() + days * 24 * 3600 * 1000);
+};
+
+Date.prototype.clone = function()
+{
+    var date =new Date();
+    date.setTime(this.getTime());
+    return date;
+};
+
+Date.isLeapYear = function (year)
+{
+    return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0));
+};
+
+Date.getDaysInMonth = function (year, month)
+{
+    return [31, (Date.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+};
+
+Date.prototype.isLeapYear = function ()
+{
+    var y = this.getFullYear();
+    return (((y % 4 === 0) && (y % 100 !== 0)) || (y % 400 === 0));
+};
+
+Date.prototype.getDaysInMonth = function ()
+{
+    return Date.getDaysInMonth(this.getFullYear(), this.getMonth());
+};
+
+Date.prototype.addMonths = function (value)
+{
+    var n = this.getDate();
+    this.setDate(1);
+    this.setMonth(this.getMonth() + value);
+    this.setDate(Math.min(n, this.getDaysInMonth()));
+};
