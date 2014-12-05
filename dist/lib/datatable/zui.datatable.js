55b181bf436cbf5201278c528f6b85eb547eba9f
Catouse <catouse@gmail.com>
2014-12-05 19:16:19 +0800

    * fix alert js file path in package.json and rebuilt zui 1.2.1.

--- dist/lib/datatable/zui.datatable.js
+++ dist/lib/datatable/zui.datatable.js
@@ -1,5 +1,5 @@
 /*!
- * ZUI - v1.2.0 - 2014-11-26
+ * ZUI - v1.2.1 - 2014-12-05
  * http://zui.sexy
  * GitHub: https://github.com/easysoft/zui.git 
  * Copyright (c) 2014 cnezsoft.com; Licensed MIT
