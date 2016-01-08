7cc5ac8f5d5c54b092ad0695e1387974f2c49d3d
Catouse <catouse@gmail.com>
2016-01-08 13:48:18 +0800

    * add chatjs header.

--- src/js/chart.line.js
+++ src/js/chart.line.js
@@ -1,3 +1,13 @@
+/*!
+ * Chart.js
+ * http://chartjs.org/
+ * Version: 1.0.2
+ *
+ * Copyright 2015 Nick Downie
+ * Released under the MIT license
+ * https://github.com/nnnick/Chart.js/blob/master/LICENSE.md
+ */
+
 /// ----- ZUI change begin -----
 /// Add jquery object to namespace
 
