cf231b4801249f5b124fe20f957bdb549059c882
Catouse <catouse@gmail.com>
2016-04-11 08:40:37 +0800

    * tree support edit actions.

--- src/js/tree.js
+++ src/js/tree.js
@@ -19,7 +19,42 @@
 
         this.getOptions(options);
         this.init();
+        console.log('this.tree', this);
     };
+
+    var DETAULT_ACTIONS = {
+        sort: {
+            trigger: '.sort-handler',
+            html: '<a class="sort-handle" href=""><i class="icon icon-move"></i></a>'
+        },
+    };
+
+    function formatActions(actions, parentActions) {
+        if(actions === true) {
+            actions = {add: true, "delete": true, edit: true, sort: true};
+        } else if(typeof actions === 'string') {
+            actions = actions.split(',');
+        }
+        if($.isArray(actions)) {
+            var _actions = {};
+            $.each(actions, function(idx, action) {
+                if($.isPlainObject(action)) {
+                    _actions[action.action] = action;
+                } else {
+                    _actions[action] = true;
+                }
+            });
+            actions = _actions;
+        }
+        if($.isPlainObject(actions)) {
+            var _actions = {};
+            $.each(actions, function(name, action) {
+                if(action) _actions[name] = $.expand({}, DETAULT_ACTIONS[name], $.isPlainObject(action) ? action : null);
+            });
+            actions = _actions;
+        }
+        return parentActions ? $.expand(true, {}, parentActions, actions) : actions;
+    }
 
     // default options
     Tree.DEFAULTS = {
@@ -27,55 +62,120 @@
         initialState: 'normal' // 'normal' | 'preserve' | 'expand' | 'collapse',
     };
 
-    Tree.prototype.init = function() {
-        if(this.options.animate) this.$.addClass('tree-animate');
+    Tree.prototype.add = function($e, items) {
+        $e = $($e);
+        if($e.is('li')) {
+            var $ul = $('<ul/>');
+            $e.addClass('')
+        } else if($e.is('ul')) {
 
-        this.$lists = this.$.find('ul');
-        this.$lists.parent('li').addClass('has-list').prepend('<i class="list-toggle icon"></i>');
-
-        var that = this;
-        this.$.on('click', '.list-toggle, a[href=#]', function(e) {
-            that.toggle($(this).parent('li'));
-            e.preventDefault();
-        });
-
-        var initialState = this.options.initialState;
-
-        if(initialState === 'preserve' && (!$.zui || !$.zui.store || !$.zui.store.enable)) {
-            initialState = 'normal';
-        }
-
-        if(initialState === 'expand') {
-            this.expand();
-        } else if(initialState === 'collapse') {
-            this.collapse();
-        } else if(initialState === 'preserve') {
-            this.isPreserve = true;
-            this.selector = name + '::' + (this.options.name || '') + '#' + (this.$.attr('id') || globalId++);
-            this.store = $.zui.store[this.options.name ? 'get' : 'pageGet'](this.selector, {});
-            function markLevel($ul, level) {
-                if(!$ul.length) return;
-                level = level || 1;
-                var idx = 1;
-                $ul.attr('data-id', level).children('li').each(function() {
-                    var $li = $(this);
-                    var id = level + '-' + idx;
-                    $li.attr('data-id', id);
-                    if(that.store.time) that[that.store[id] ? 'expand' : 'collapse']($li, true, true);
-                    markLevel($li.children('ul').first(), id);
-                    idx++;
-                });
-            }
-            markLevel(this.$);
-        }
-
-        if(this.options.animate) {
-            this.$.find('li.has-list.open').addClass('in');
         }
     };
 
-    Tree.prototype.preserve = function($li, id, expand) {
+    Tree.prototype.init = function($e) {
+        var options = this.options, that = this;
+        if(!$e) {
+            this.init(this.$.addClass('tree'));
+            if(options.animate) this.$.addClass('tree-animate');
+            this.$.on('click', '.list-toggle, a[href=#]', function(e) {
+                var $li = $(this).parent('li');
+                that.callEvent('hit', {target: $li, item: $li.data('data')});
+                that.toggle($li);
+                e.preventDefault();
+            });
+            var initialState = options.initialState;
+            if(initialState === 'preserve' && (!$.zui || !$.zui.store || !$.zui.store.enable)) {
+                initialState = 'normal';
+            }
+            if(initialState === 'expand') {
+                this.expand();
+            } else if(initialState === 'collapse') {
+                this.collapse();
+            } else if(initialState === 'preserve') {
+                this.isPreserve = true;
+                this.selector = name + '::' + (options.name || '') + '#' + (this.$.attr('id') || globalId++);
+                this.store = $.zui.store[options.name ? 'get' : 'pageGet'](this.selector, {});
+
+                function markLevel($ul, level) {
+                    if(!$ul.length) return;
+                    level = level || 1;
+                    var idx = 1;
+                    $ul.attr('data-id', level).children('li').each(function() {
+                        var $li = $(this);
+                        var id = level + '-' + idx;
+                        $li.attr('data-id', id);
+                        if(that.store.time) that[that.store[id] ? 'expand' : 'collapse']($li, true, true);
+                        markLevel($li.children('ul').first(), id);
+                        idx++;
+                    });
+                }
+                markLevel(this.$);
+            }
+        } else if($e.is('ul')) {
+
+
+        } else if($e.is('li')) {
+
+        }
+    };
+
+    Tree.prototype.init = function() {
+        this.$.addClass('tree');
+        var options = this.options;
+        if(options.animate) this.$.addClass('tree-animate');
+
+        // init data
+        if(options.data) {
+            function buildTree($ul, items) {
+                $.each(items, function(idx, item) {
+                    var $li = $('<li/>').data(item).append($('<a/>', {href: item.url || '#'}).text(item.title));
+                    if(item.children) {
+                        $li.append(buildTree($('<ul>').toggleClass('open in', item.open), item.children));
+                    }
+                    $ul.append($li);
+                });
+                return $ul;
+            }
+            buildTree(this.$, options.data);
+        }
+
+        this.$.find('ul').parent('li').addClass('has-list').prepend('<i class="list-toggle icon"></i>');
+
         var that = this;
+        this.$.on('click', '.list-toggle, a[href=#]', function(e) {
+            var $li = $(this).parent('li');
+            that.callEvent('hit', {target: $li, item: $li.data('data')});
+            that.toggle($li);
+            e.preventDefault();
+        });
+
+        if(options.animate) {
+            this.$.find('li.has-list.open').addClass('in');
+        }
+
+        var actions = formatActions(options.actions);
+        if($.isPlainObject(actions)) {
+            this.actions = actions;
+            this._initActionsUI(this.$.find('li'));
+        }
+
+        if(options.sortable) {
+            var sortOptions = {trigger: '.sort-handler'};
+            if($.isPlainObject(options.sortable)) $.expand(sortOptions, options.sortable);
+            this.$.find('li').find('a').after('<i class="icon icon-move sort-handler"></i>');
+            this.$.sortable(sortOptions).find('ul').sortable(sortOptions);
+        }
+    };
+
+    Tree.prototype._initActionsUI = function(li, actions) {
+        actions = actions || this.actions;
+        $(li).each(function() {
+
+        });
+    };
+
+    Tree.prototype.preserve = function($li, id, expand) {
+        if(!this.isPreserve) return;
         if($li) {
             id = id || $li.data('id');
             expand = expand === undefined ? $li.hasClass('open') : false;
@@ -84,6 +184,7 @@
             this.store.time = new Date();
             $.zui.store[this.options.name ? 'set' : 'pageSet'](this.selector, this.store);
         } else {
+            var that = this;
             this.store = {};
             this.$.find('li').each(function() {
                 that.store($(this));
