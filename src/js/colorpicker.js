0d80200587e5111a77ae3f155932b94935c02c8b
Catouse <catouse@gmail.com>
2016-04-15 09:39:55 +0800

    * change copyright year.

--- src/js/colorpicker.js
+++ src/js/colorpicker.js
@@ -2,7 +2,7 @@
  * ZUI: ColorPicker.js
  * http://zui.sexy
  * ========================================================================
- * Copyright (c) 2014 cnezsoft.com; Licensed MIT
+ * Copyright (c) 2016 cnezsoft.com; Licensed MIT
  * ======================================================================== */
 
 
