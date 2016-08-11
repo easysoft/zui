117f6df496e3921f908bacc4d89909839514161b
Catouse <catouse@gmail.com>
2016-08-11 15:07:09 +0800

    * change browser support.

--- gulpfile.js
+++ gulpfile.js
@@ -334,7 +334,14 @@ function buildBundle(name, callback, type) {
             return gulp.src(buildSourceFilePath)
                 .pipe(less())
                 .pipe(autoprefixer({
-                    browsers: ['> 1%'],
+                    browsers: ["Android 2.3",
+                        "Android >= 4",
+                        "Chrome >= 20",
+                        "Firefox >= 24",
+                        "Explorer >= 8",
+                        "iOS >= 6",
+                        "Opera >= 12",
+                        "Safari >= 6"],
                     cascade: true
                 }))
                 .pipe(csscomb())
