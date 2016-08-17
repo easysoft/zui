3b85ea9c2a544e4a852bb6bcf8ab79a9a418796d
Catouse <catouse@gmail.com>
2016-08-17 17:05:18 +0800

    * improve document UI.

--- src/js/doc.js
+++ src/js/doc.js
@@ -72,7 +72,7 @@
         pkgLibs = {
             standard: null,
             lite: null,
-            separate: null
+            seperate: null
         };
     var docThemes = {
         "default": {
@@ -1148,7 +1148,7 @@
         if(lib) {
             $pageAttrs.children('.badge-zui').toggle(!!lib.bundles.standard);
             $pageAttrs.children('.badge-lite').toggle(!!lib.bundles.lite);
-            $pageAttrs.children('.badge-lib').toggle(!!lib.bundles.separate);
+            $pageAttrs.children('.badge-lib').toggle(!!lib.bundles.seperate);
             $pageAttrs.children('.badge-custom').toggle(!!lib.custom);
 
             $pageAttrs.children('.badge-version').toggle(!!lib.ver).text(lib.ver + '+');
@@ -1228,7 +1228,7 @@
                             $dropdown.append('<li><a target="_blank" href="https://github.com/easysoft/zui/blob/master/' + f.replace('/**/*', '') + '">' + f + '</a></li>');
                         });
                     }
-                    if(lib.bundles.separate) {
+                    if(lib.bundles.seperate) {
                         $dropdown.append('<li class="dropdown-header">独立组件</li>');
                         $dropdown.append('<li><a target="_blank" href="https://github.com/easysoft/zui/blob/master/dist/lib/' + section.id + '">dist/lib/' + section.id + '/**/*</a></li>');
                     }
@@ -1494,12 +1494,25 @@
         }
 
         if(build.bundles) {
-            $.each(build.bundles, function(idx, val) {
-                if(pkg.builds[val]) {
-                    getBuildList(pkg, pkg.builds[val], lib, list);
-                } else {
-                    list = getItemList(lib, [val], list);
+            $.each(build.bundles, function(idx, name) {
+                var bundleBuild = pkg.builds[name];
+                var buildLib = lib[name];
+                if(!bundleBuild && buildLib) {
+                    bundleBuild = {
+                        title: buildLib.name,
+                        dest: 'dist/lib/' + name + '/',
+                        filename: (buildLib.source && buildLib.source !== 'Bootstrap') ? name : ('zui.' + name),
+                        includes: [name],
+                        source: buildLib.source,
+                        settingDpds: (buildLib.src && buildLib.src.less && buildLib.src.less.length) ? ['setting'] : null,
+                        ignoreBasic: true,
+                        ignoreDpds: buildLib.ignoreDpds === undefined ? true : buildLib.ignoreDpds
+                    };
+                    pkg.builds[name] = bundleBuild;
+                    console.log('> bundleBuild', bundleBuild);
                 }
+
+                getBuildList(pkg, bundleBuild, lib, list);
             });
         }
 
@@ -1549,7 +1562,9 @@
             $('.zui-version').text('v' + pkg.version);
             pkgLibs.standard = getBuildList(pkg, pkg.builds.standard, pkg.lib);
             pkgLibs.lite = getBuildList(pkg, pkg.builds.lite, pkg.lib);
-            pkgLibs.separate = getBuildList(pkg, pkg.builds.separate, pkg.lib);
+            pkgLibs.seperate = getBuildList(pkg, pkg.builds.seperate, pkg.lib);
+
+            console.log('pkgLibs', pkgLibs);
 
             function getLibSource(lib, src, libName) {
                 if(lib.src) {
@@ -1624,7 +1639,7 @@
                     lib.bundlesCount = 0;
                     if(!!lib.bundles.standard) lib.bundlesCount++;
                     if(!!lib.bundles.lite) lib.bundlesCount++;
-                    if(!!lib.bundles.separate) lib.bundlesCount++;
+                    if(!!lib.bundles.seperate) lib.bundlesCount++;
                     if(lib.code === 'theme') lib.bundlesCount++;
                 }
 
@@ -1641,15 +1656,18 @@
             var getChildCompsList = function(val) {
                 return data.lib[val].name;
             };
-            var $tr, $td;
+            var $tr, $td, totalCount = 0;
             $.each(data.lib, function(itemName, item) {
-                if(item.custom) return;
+                if(item.hidden) return;
 
                 var childComps = '';
                 if(!item.src && item.dpds) {
                     var childList = getItemList(data.lib, item.dpds, null, true, true);
-                    childComps = '合并组件包含：';
+                    childComps = '包含：';
                     childComps += $.map(childList, getChildCompsList).join('、');
+                    item.merged = true;
+                } else {
+                    totalCount++;
                 }
 
                 $tr = $('<tr/>');
@@ -1670,6 +1688,21 @@
                 });
 
                 $td = $('<td/>');
+                if(item.source) {
+                    var $a = $('<a/>', {
+                        target: '_blank',
+                        title: 'License: ' + item.license,
+                        href: item.website || item.project || (item.source === 'Bootstrap' ? 'http://getbootstrap.com/' : '###')
+                    }).text(item.source);
+                    $td.append($a);
+                } else if(item.merged) {
+                    $td.append('<span class="text-muted">(合并组件)</span>');
+                } else {
+                    $td.append('ZUI');
+                }
+                $tr.append($td);
+
+                $td = $('<td/>');
                 $td.html(item.ver ? (' v' + item.ver + '+') : childComps);
                 $tr.append($td);
 
@@ -1681,6 +1714,7 @@
                 rowHover: false,
                 fixedHeaderOffset: 200
             });
+            $('.components-count').text(totalCount);
         });
     };
 
