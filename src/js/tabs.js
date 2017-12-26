7d526207f10fb8ef5c16cbb9289e7614f7c9b30b
Catouse <catouse@gmail.com>
2017-12-26 19:45:44 +0800

    * improve UI of tabs.

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -60,12 +60,18 @@
     var DEFAULTS = {
         tabs: [],
         defaultTabIcon: 'icon-window',
+        contextMenu: true,
+        errorTemplate: '<div class="alert alert-block alert-danger with-icon"><i class="icon-warning-sign"></i><div class="content">{0}</div></div>'
         // messagerOptions: null,
     };
 
     var LANG = {
         zh_cn: {
+            reload: '重新加载',
             close: '关闭',
+            closeOthers: '关闭其他标签页',
+            closeRight: '关闭右侧标签页',
+            reopenLast: '恢复上次关闭的标签页',
             errorCannotFetchFromRemote: '无法从远程服务器（{0}）获取内容。'
         }
     };
@@ -76,7 +82,7 @@
         that.name = NAME;
         that.$ = $(element);
 
-        options = that.options = $.extend({}, Tabs.DEFAULTS, this.$.data(), options);
+        options = that.options = $.extend({}, DEFAULTS, this.$.data(), options);
         var lang   = options.lang || 'zh_cn';
         that.lang  = $.isPlainObject(lang) ? ($.extend(true, {}, LANG[lang.lang || $.zui.clientLang()], lang)) : LANG[lang];
 
@@ -89,7 +95,7 @@
 
         var $nav = $navbar.find('.tabs-nav');
         if (!$nav.length) {
-            $nav = $('<ul class="tabs-nav"></ul>').appendTo($navbar);
+            $nav = $('<ul class="tabs-nav nav nav-tabs"></ul>').appendTo($navbar);
         }
         that.$nav = $nav;
 
@@ -119,9 +125,59 @@
         $nav.on('click.' + NAME, '.tab-nav-link', function () {
             that.open(that.getTab($(this).data('id')));
         }).on('click.' + NAME, '.tab-nav-close', function (e) {
-            that.close($(this).closest('.tab-nav-item').data('id'));
+            that.close($(this).closest('.tab-nav-link').data('id'));
             e.stopPropagation();
+        }).on('resize.' + NAME, function () {
+            that.adjustNavs();
         });
+
+        if (options.contextMenu) {
+            $nav.contextmenu({
+                selector: '.tab-nav-link',
+                itemsCreator: function (e) {
+                    return that.createMenuItems(that.getTab($(this).data('id')));
+                },
+                onShow: function () {
+                    that.$.addClass('tabs-show-contextmenu');
+                },
+                onHide: function () {
+                    that.$.removeClass('tabs-show-contextmenu');
+                }
+            });
+        }
+    };
+
+    Tabs.prototype.createMenuItems = function (tab) {
+        var that = this;
+        var lang = that.lang;
+        return [{
+            label: lang.reload,
+            onClick: function () {
+                that.open(tab, true);
+            }
+        }, '-', {
+            label: lang.close,
+            disabled: tab.forbidClose,
+            onClick: function () {
+                that.close(tab.id);
+            }
+        }, {
+            label: lang.closeOthers,
+            onClick: function () {
+                that.closeOthers(tab.id);
+            }
+        }, {
+            label: lang.closeRight,
+            onClick: function () {
+                that.closeRight(tab.id);
+            }
+        }, '-', {
+            label: lang.reopenLast,
+            disabled: that.closedTabs.length,
+            onClick: function () {
+                that.reopen();
+            }
+        }];
     };
 
     Tabs.prototype.adjustNavs = function (immediately) {
@@ -155,11 +211,11 @@
         var $nav = that.$nav;
         var $tabNav = $('#tab-nav-item-' + tab.id);
         if (!$tabNav.length) {
-            var $a = $('<a class="tab-nav-link"><i class="icon"></i><span class="text"></span><i class="close tab-nav-close" title="' + that.lang.close + '"></i></a>').attr({
+            var $a = $('<a class="tab-nav-link"><i class="icon"></i><span class="title"></span><i class="close tab-nav-close" title="' + that.lang.close + '">&times;</i></a>').attr({
                 href: '#tabs-item-' + tab.id,
-                id: tab.id
+                'data-id': tab.id
             });
-            $tabNav = $('<li class="tab-nav-item" id="tab-nav-item-' + tab.id + '" />').append($a).appendTo(that.$nav);
+            $tabNav = $('<li class="tab-nav-item" data-id="' + tab.id + '" id="tab-nav-item-' + tab.id + '" />').append($a).appendTo(that.$nav);
             if (beforeTabId) {
                 var $before$nav = $('#tab-nav-item-' + beforeTabId);
                 if ($before$nav.length) {
@@ -168,9 +224,9 @@
             }
             that.adjustNavs();
         }
-        var $a = $tabNav.find('a').attr('title', tab.desc);
+        var $a = $tabNav.find('a').attr('title', tab.desc).toggleClass('not-closable', !!tab.forbidClose);
         $a.find('.icon').attr('class', 'icon ' + (tab.icon || that.options.defaultTabIcon));
-        $a.find('.text').text(tab.title || '');
+        $a.find('.title').text(tab.title || tab.defaultTitle || '');
         return $tabNav;
     };
 
@@ -193,28 +249,32 @@
     Tabs.prototype.close = function(tabId) {
         var that = this;
         var tab = that.getTab(tabId);
-        if (tab) {
+        if (tab && !tab.forbidClose) {
             $('#tab-nav-item-' + tab.id).remove();
             $('#tab-' + tab.id).remove();
             tab.close();
             delete that.tabs[tab.id];
             that.closedTabs.push(tab);
             that.$.callComEvent(that, 'onClose', tab);
-        }
 
-        var lastTab;
-        $.each(that.tabs, function (tabId, tab) {
-            if (!lastTab || lastTab.openTime < tab.openTime) {
-                lastTab = tab;
-            }
-        });
-        lastTab && that.open(lastTab);
+            var lastTab;
+            $.each(that.tabs, function (tabId, tab) {
+                if (!lastTab || lastTab.openTime < tab.openTime) {
+                    lastTab = tab;
+                }
+            });
+            lastTab && that.open(lastTab);
+        }
     };
 
-    Tabs.prototype.open = function(tab) {
+    Tabs.prototype.open = function(tab, forceReload) {
         var that = this;
 
-        var $tabNav = that.render(tab);
+        if (!(tab instanceof Tab)) {
+            tab = Tab.create(tab);
+        }
+
+        var $tabNav = that.renderTab(tab);
         that.$nav.find('.tab-nav-item.active').removeClass('active');
         $tabNav.addClass('active');
 
@@ -226,8 +286,10 @@
         $tabPane.addClass('active');
 
         tab.open();
+        that.activeTabId = tab.id;
+        that.tabs[tab.id] = tab;
 
-        if (!tab.loaded) {
+        if (forceReload || !tab.loaded) {
             that.reload(tab);
         }
 
@@ -244,6 +306,11 @@
 
     Tabs.prototype.reload = function(tab) {
         var that = this;
+
+        if (!tab.openTime) {
+            return that.open(tab);
+        }
+
         var $tabNav = $('#tab-nav-item-' + tab.id).addClass('loading').removeClass('has-error');
         var $tabPane = $('#tab-' + tab.id).addClass('loading').removeClass('has-error');
         var afterRefresh = function (content, error) {
@@ -255,13 +322,21 @@
             that.$.callComEvent(that, 'onRefresh', tab);
             if(typeof content === 'string') {
                 $tabPane.html(content);
+                if (!tab.title) {
+                    content = $tabPane.text().replace(/\n/g, '');
+                    tab.title = content.length > 10 ? content.substr(0, 10) : content;
+                    that.renderTab(tab);
+                }
             }
             if (error) {
                 $tabNav.addClass('has-error');
                 $tabPane.addClass('has-error');
                 that.showMessage(error, 'danger');
+                if (!content) {
+                    $tabPane.html(that.options.errorTemplate.format(error));
+                }
             }
-            tan.loaded = new Date().getTime();
+            tab.loaded = new Date().getTime();
         };
         if (tab.type === 'ajax') {
             var ajaxOption = {
@@ -282,11 +357,17 @@
             try {
                 var iframeName = 'tab-iframe-' + tab.id;
                 var $iframe = $('<iframe id="' + iframeName + '" name="' + iframeName + '" src="' + (tab.url) + '" frameborder="no"  allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"  allowtransparency="true" scrolling="auto" style="width: 100%; height: 100%; left: 0px;"></iframe>');
-                $iframe.appendTo($tabPane);
+                $iframe.appendTo($tabPane.empty());
+                $('<div class="tab-iframe-cover" />').appendTo($tabPane);
                 var frame = document.getElementById(iframeName);
                 frame.onload = frame.onreadystatechange = function() {
                     if(this.readyState && this.readyState != 'complete') return;
                     afterRefresh();
+                    var title = frame.contentDocument.title;
+                    if (!tab.title && title) {
+                        tab.title = title;
+                        that.renderTab(tab);
+                    }
                 };
             } catch (e) {
                 afterRefresh();
@@ -305,10 +386,11 @@
     };
 
     Tabs.prototype.closeOthers = function(tabId) {
-        $nav.find('.tab-nav-item:not(.hidden)').each(function() {
+        var that = this;
+        that.$nav.find('.tab-nav-link:not(.hidden)').each(function() {
             var thisTabId = $(this).data('id');
             if (thisTabId !== tabId) {
-                that.close(tabId);
+                that.close(thisTabId);
             }
         });
     };
@@ -316,19 +398,21 @@
     Tabs.prototype.closeRight = function(tabId) {
         var $tabNav = $('#tab-nav-item-' + tabId);
         var $rightNav = $tabNav.next('.tab-nav-item:not(.hidden)');
-        while($rightNav.length) {
-            that.close($rightNav.data('id'));
+        while ($rightNav.length) {
+            this.close($rightNav.data('id'));
             $rightNav = $tabNav.next('.tab-nav-item:not(.hidden)');
         }
     };
 
     Tabs.prototype.closeAll = function() {
-        $nav.find('.tab-nav-item:not(.hidden)').each(function() {
+        var that = this;
+        that.$nav.find('.tab-nav-link:not(.hidden)').each(function() {
             that.close($(this).data('id'));
         });
     };
 
     Tabs.prototype.reopen = function() {
+        var that = this;
         if(that.closedTabs.length) {
             that.open(that.closedTabs.pop());
         }
