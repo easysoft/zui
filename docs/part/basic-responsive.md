3ea68e76d875795fd938dd2d952200d2c6d5b587
Catouse <catouse@gmail.com>
2017-12-27 10:55:07 +0800

    * support detect os type.

--- docs/part/basic-responsive.md
+++ docs/part/basic-responsive.md
@@ -9,6 +9,8 @@ filter: xiangyingshibuju xysbj
 
 现代 Web 应用应该支持响应式设计。栅格系统已提供良好的基础构建响应式设计页面，同时也提供一些辅助工具类来控制内容在不同设备的展现方式。
 
+## 屏幕尺寸响应
+
 在ZUI中提供针对 4 中不同尺寸的设备屏幕进行分别控制。
 
 <table class="table table-bordered">
@@ -123,6 +125,87 @@ ZUI也提供用来控制打印机的显示与隐藏的辅助类。显示和隐�
 *   `.hidden-print`：在浏览器正常浏览时显示，在打印时隐藏。
 
 默认情况下，响应式显示辅助类 `.visible-*` 会将元素的 CSS `display` 属性设置为 `block`（表格元素 `<tr>`、`<td>`、`<th>` 除外）。如果期望在显示时将 `display` 属性设置为 `inline` 或者 `inline-block`，应该将 `.visible-*` 与 `.inline` 或 `.inline-block` 一起使用。
+
+在页面加载完成和页面窗口宽度变化时，`<html>` 标签上还会自动添加以下辅助类用于为不同的屏幕尺寸应用外观（此功能需要 ZUI 的 JavaScript 实现）：
+
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th>屏幕尺寸</th>
+      <th><code>html</code> 标签上的辅助类</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>屏幕宽度&lt;768px</td>
+      <td><code>.screen-phone</code></td>
+    </tr>
+    <tr>
+      <td>768px&lt;=屏幕宽度&lt;992px</td>
+      <td><code>.screen-tablet</code></td>
+    </tr>
+    <tr>
+      <td>992px&lt;=屏幕宽度&lt;1200px</td>
+      <td><code>.screen-desktop</code></td>
+    </tr>
+    <tr>
+      <td>屏幕宽度&gt;=1200px</td>
+      <td><code>.screen-desktop-wide</code></td>
+    </tr>
+    <tr>
+      <td>屏幕宽度&gt;=992px</td>
+      <td><code>.device-desktop</code></td>
+    </tr>
+    <tr>
+      <td>屏幕宽度&lt;992px</td>
+      <td><code>.device-mobile</code></td>
+    </tr>
+  </tbody>
+</table>
+
+## 设备类型和系统检测
+
+针对不同的操作系统，会在页面加载时为 `<html>` 标签自动添加以下辅助类方便为不同的设备系统应用不同的样式（此功能需要 ZUI 的 JavaScript 实现）：
+
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th>系统和功能类型</th>
+      <th><code>html</code> 标签上的辅助类</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Windows 操作系统</td>
+      <td><code>.os-windows</code></td>
+    </tr>
+    <tr>
+      <td>Mac 操作系统</td>
+      <td><code>.os-mac</code></td>
+    </tr>
+    <tr>
+      <td>Android 系统</td>
+      <td><code>.os-android</code></td>
+    </tr>
+    <tr>
+      <td>iOS 系统</td>
+      <td><code>.os-ios</code></td>
+    </tr>
+    <tr>
+      <td>Linux 系统</td>
+      <td><code>.os-linux</code></td>
+    </tr>
+    <tr>
+      <td>Unix 系统</td>
+      <td><code>.os-unix</code></td>
+    </tr>
+    <tr>
+      <td>触摸屏设备</td>
+      <td><code>.is-touchable</code></td>
+    </tr>
+  </tbody>
+</table>
+
 
 ## 禁用响应式设计
 
--- src/js/device.js
+++ src/js/device.js
@@ -10,28 +10,47 @@
     'use strict';
     var desktopLg = 1200,
         desktop = 992,
-        tablet = 768,
-        cssNames = {
-            desktop: 'screen-desktop',
-            desktopLg: 'screen-desktop-wide',
-            tablet: 'screen-tablet',
-            phone: 'screen-phone',
-            isMobile: 'device-mobile',
-            isDesktop: 'device-desktop',
-            touch: 'is-touchable'
-        };
+        tablet = 768;
 
     var $window = $(window);
 
     var resetCssClass = function() {
         var width = $window.width();
-        $('html').toggleClass(cssNames.desktop, width >= desktop && width < desktopLg)
-            .toggleClass(cssNames.desktopLg, width >= desktopLg)
-            .toggleClass(cssNames.tablet, width >= tablet && width < desktop)
-            .toggleClass(cssNames.phone, width < tablet)
-            .toggleClass(cssNames.isMobile, width < desktop)
-            .toggleClass(cssNames.touch, 'ontouchstart' in document.documentElement)
-            .toggleClass(cssNames.isDesktop, width >= desktop);
+        var classNames = '';
+
+        if (width >= desktopLg) {
+            classNames += ' screen-desktop-wide';
+        } else if (width >= desktop && width < desktopLg) {
+            classNames += ' screen-desktop';
+        } else if (width >= tablet && width < desktop) {
+            classNames += ' screen-tablet';
+        } else {
+            classNames += ' screen-phone';
+        }
+        if (width < desktop) {
+            classNames += ' device-mobile';
+        } else {
+            classNames += ' device-desktop';
+        }
+        if ('ontouchstart' in document.documentElement) {
+            classNames += ' is-touchable';
+        }
+
+        var userAgent = navigator.userAgent;
+        if (userAgent.match(/(iPad|iPhone|iPod)/i)) {
+            classNames += ' os-ios';
+        } else if (userAgent.match(/android/i)) {
+            classNames += ' os-android';
+        } else if (userAgent.match(/Win/i)) {
+            classNames += ' os-windows';
+        } else if (userAgent.match(/Mac/i)) {
+            classNames += ' os-mac';
+        } else if (userAgent.match(/Linux/i)) {
+            classNames += ' os-linux';
+        } else if (userAgent.match(/X11/i)) {
+            classNames += ' os-unix';
+        }
+        $('html').addClass(classNames);
     };
 
     $window.resize(resetCssClass);
