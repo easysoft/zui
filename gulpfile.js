fb4e31ff4b4b433028ea35bb1a0f145000af2a23
Catouse <catouse@gmail.com>
2018-03-16 16:33:00 +0800

    * change gulpfile to catch errors without stopping gulp.watch.

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,31 @@ var extend = require('extend'),
 // Disable the 'possible EventEmitter memory leak detected' warning.
 gulp.setMaxListeners(0);
 
+// Workaround for https://github.com/gulpjs/gulp/issues/71#issuecomment-68062782
+var origSrc = gulp.src;
+gulp.src = function () {
+    return fixPipe(origSrc.apply(this, arguments));
+};
+function fixPipe(stream) {
+    var origPipe = stream.pipe;
+    stream.pipe = function (dest) {
+        arguments[0] = dest.on('error', function (error) {
+            var nextStreams = dest._nextStreams;
+            if (nextStreams) {
+                nextStreams.forEach(function (nextStream) {
+                    nextStream.emit('error', error);
+                });
+            } else if (dest.listeners('error').length === 1) {
+                throw error;
+            }
+        });
+        var nextStream = fixPipe(origPipe.apply(this, arguments));
+        (this._nextStreams || (this._nextStreams = [])).push(nextStream);
+        return nextStream;
+    };
+    return stream;
+}
+
 extend(true, zui, require('./zui.templates.json'));
 
 // try load zui.custom.json and merge into zui.
