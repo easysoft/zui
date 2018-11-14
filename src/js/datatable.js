78bbb37f9b329d5bd8a2af6b286bf5623bd1ea99
Catouse <catouse@gmail.com>
2018-11-14 15:32:28 +0800

    * fix title attribute of column in datatable not work.

--- src/js/datatable.js
+++ src/js/datatable.js
@@ -150,7 +150,8 @@
                         type: 'string',
                         ignore: $th.hasClass('ignore'),
                         sort: !$th.hasClass('sort-disabled'),
-                        mergeRows: $th.attr('merge-rows')
+                        mergeRows: $th.attr('merge-rows'),
+                        title: $th.attr('title')
                     }, $th.data()));
                 });
 
@@ -286,7 +287,8 @@
                 .attr({
                     'data-index': i,
                     'data-type': col.type,
-                    style: col.css
+                    style: col.css,
+                    title: col.title,
                 }).css('width', col.width);
             $tr.append($th);
         }
