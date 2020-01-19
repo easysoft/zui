68c7d9b860f2ea3dae56d67e12b18873e9e7eaf1
Catouse <catouse@gmail.com>
2020-01-19 10:18:20 +0800

    * update ja language data file.

--- src/js/lang/ja.js
+++ src/js/lang/ja.js
@@ -21,7 +21,7 @@ $.zui.lang('ja', {
     },
     'zui.calendar': {
         weekNames: ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日'],
-        monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
         today: '今日',
         year: '{0}年',
         month: '{0}月',
@@ -67,7 +67,7 @@ $.zui.lang('ja', {
         meridiem: []
     },
     chosen: {
-        no_results_text: 'Pas trouvé'
+        no_results_text: '見つかりません'
     },
     bootbox: {
         OK: 'オーケー',
