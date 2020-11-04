efa012f27d372a776b083cac38ecd8c3954221a2
Tintin <istintin@qq.com>
2020-11-04 15:34:51 +0800

    * add template pages.

--- template/src/page/datagrid/index.js
+++ template/src/page/datagrid/index.js
@@ -1,2 +1,60 @@
+import './less/index.less';
 import $ from 'jquery';
-$('#tableDataGridExample').datagrid();
+
+function createIndex(index) {
+  return `<div class="p p${index}">${index}</div>`;
+}
+
+const arr = [];
+for(let i = 0; i < 200; i++) {
+  arr.push({
+    id: 200 + i,
+    p: createIndex([1,2,3][Math.floor(Math.random()*3)]),
+    name: `<a href="#">${['系统升级', '系统优化', '修复BUG', '打包发布', '功能测试'][Math.floor(Math.random()*5)]}</a>`,
+    project: ['', '1.0', '2.0'][Math.floor(Math.random()*3)],
+    createBy: ['李雷', '韩梅梅', '孙鑫', '张凯'][Math.floor(Math.random()*4)],
+    assign: ['李雷', '韩梅梅', '<a href="#">未指派</a>', '张凯'][Math.floor(Math.random()*4)],
+    expected: Math.ceil(Math.random()*9),
+    status: ['未激活', '激活', '<span style="color: #8666b8">草稿</span>'][Math.floor(Math.random()*3)],
+    stage: ['未开始', '已立项', '研发完毕', '已计划'][Math.floor(Math.random()*4)],
+    t: ['0', '<a href="#">1</a>'][Math.floor(Math.random()*2)],
+    action: `
+    <a href="#" class="action-btn" title="开始"><i class="icon icon-play"></i></a>
+    <a href="#" class="action-btn" title="完成"><i class="icon icon-check"></i></a>
+    <a href="#" class="action-btn" title="日志"><i class="icon icon-time"></i></a>
+    <a href="#" class="action-btn" title="编辑任务"><i class="icon icon-edit"></i></a>
+    <a href="#" class="action-btn" title="子任务"><i class="icon icon-sitemap"></i></a>
+    `
+  });
+}
+
+$('#datagridExample').datagrid({
+  height: 490,
+  dataSource: {
+    cols: [
+      {name: 'id', label: 'ID', sort: true, checkbox: true},
+      {name: 'p', label: 'P', sort: true, html: true},
+      {name: 'name', label: '需求名称', sort: true, html: true},
+      {name: 'project', label: '计划', sort: true},
+      {name: 'createBy', label: '创建', sort: true},
+      {name: 'assign', label: '指派', sort: true, html: true},
+      {name: 'expected', label: '预计', sort: true},
+      {name: 'status', label: '状态', sort: true, html: true},
+      {name: 'stage', label: '阶段', sort: true},
+      {name: 't', label: 'T', sort: false, html: true},
+      {name: 'action', label: '操作', sort: false, html: true, width: 200, style: {textAlign: 'center'}}
+    ],
+    array: arr,
+  },
+  sortable: true,
+  states: {
+    pager: {
+      page: 1,
+      recPerPage: 20,
+      fixedTopUntil: 0,
+    }
+  },
+  checkable: true,
+  checkByClickRow: true,
+  showRowIndex: false
+});
---
+++ template/src/page/detail/index.js
@@ -0,0 +1,147 @@
+import './less/index.less';
+import $ from 'jquery';
+
+const kuid = '5fa24ade36d6c';
+const editor = {
+    "id": ["comment", "lastComment"],
+    "tools": "simpleTools"
+};
+const K = KindEditor;
+
+const bugTools = ['formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold', 'italic',
+    'underline', '|',
+    'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist', 'insertunorderedlist', '|',
+    'emoticons', 'image', 'code', 'link', '|', 'removeformat', 'undo', 'redo', 'fullscreen', 'source',
+    'about'
+];
+const simpleTools = ['formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold', 'italic',
+    'underline', '|',
+    'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist', 'insertunorderedlist', '|',
+    'emoticons', 'image', 'code', 'link', 'table', '|', 'removeformat', 'undo', 'redo', 'fullscreen',
+    'source', 'about'
+];
+const fullTools = ['formatblock', 'fontname', 'fontsize', 'lineheight', '|', 'forecolor', 'hilitecolor', '|',
+    'bold', 'italic', 'underline', 'strikethrough', '|',
+    'justifyleft', 'justifycenter', 'justifyright', 'justifyfull', '|',
+    'insertorderedlist', 'insertunorderedlist', '|',
+    'emoticons', 'image', 'insertfile', 'hr', '|', 'link', 'unlink', '/',
+    'undo', 'redo', '|', 'selectall', 'cut', 'copy', 'paste', '|', 'plainpaste', 'wordpaste', '|',
+    'removeformat', 'clearhtml', 'quickformat', '|',
+    'indent', 'outdent', 'subscript', 'superscript', '|',
+    'table', 'code', '|', 'pagebreak', 'anchor', '|',
+    'fullscreen', 'source', 'preview', 'about'
+];
+const editorToolsMap = {
+    fullTools: fullTools,
+    simpleTools: simpleTools,
+    bugTools: bugTools
+};
+
+// Kindeditor default options
+const editorDefaults = {
+    cssPath: [config.themeRoot + 'zui/css/min.css'],
+    width: '100%',
+    height: '200px',
+    filterMode: true,
+    bodyClass: 'article-content',
+    urlType: 'absolute',
+    uploadJson: createLink('file', 'ajaxUpload', 'uid=' + kuid),
+    langType: 'zh_CN',
+    cssData: 'html,body {background: none}.article-content{overflow:visible}.article-content, .article-content table td, .article-content table th {line-height: 1.3846153846; font-size: 13px;}.article-content .table-auto {width: auto!important; max-width: 100%;}',
+    placeholder: "\u53ef\u4ee5\u5728\u7f16\u8f91\u5668\u76f4\u63a5\u8d34\u56fe\u3002",
+    placeholderStyle: {
+        fontSize: '13px',
+        color: '#888'
+    },
+    pasteImage: {
+        postUrl: createLink('file', 'ajaxPasteImage', 'uid=' + kuid)
+    },
+    syncAfterBlur: true,
+    spellcheck: false
+};
+
+window.editor = {};
+
+// Init kindeditor
+const setKindeditor = function(element, options) {
+    const $editor = $(element);
+    const editorID = $editor.attr('id');
+    options = $.extend({}, editorDefaults, $editor.data(), options);
+    if(editorID === undefined) {
+        editorID = 'kindeditor-' + $.zui.uuid();
+        $editor.attr('id', editorID);
+    }
+
+    const editorTool = editorToolsMap[options.tools || editor.tools] || simpleTools;
+
+    /* Remove fullscreen in modal. */
+    if(config.onlybody == 'yes') {
+        const newEditorTool = new Array();
+        for(i in editorTool) {
+            if(editorTool[i] != 'fullscreen') newEditorTool.push(editorTool[i]);
+        }
+        editorTool = newEditorTool;
+    }
+
+    $.extend(options, {
+        items: editorTool,
+        placeholder: $editor.attr('placeholder') || options.placeholder || '',
+        pasteImage: {
+            postUrl: createLink('file', 'ajaxPasteImage', 'uid=' + kuid),
+            placeholder: $editor.attr('placeholder') ||
+                "\u53ef\u4ee5\u5728\u7f16\u8f91\u5668\u76f4\u63a5\u8d34\u56fe\u3002"
+        },
+    });
+
+    try {
+        const keditor = K.create('#' + editorID, options);
+        window.editor['#'] = window.editor[editorID] = keditor;
+        $editor.data('keditor', keditor);
+        return keditor;
+    } catch (e) {
+        return false;
+    }
+};
+
+// Init kindeditor with jquery way
+$.fn.kindeditor = function(options) {
+    return this.each(function() {
+        setKindeditor(this, options);
+    });
+};
+
+// Init all kindeditor
+const initKindeditor = function(afterInit) {
+    const $submitBtn = $('form :input[type=submit]');
+    if($submitBtn.length) {
+        $submitBtn.next('#uid').remove();
+        $submitBtn.after("<input type='hidden' id='uid' name='uid' value=" + kuid + ">");
+    }
+    if($.isFunction(afterInit)) afterInit();
+    $.each(editor.id, function(key, editorID) {
+        setKindeditor('#' + editorID);
+    });
+};
+
+// Init all kindeditors when document is ready
+$(initKindeditor);
+
+$(function() {
+    const diffButton =
+        '<button type="button" class="btn btn-mini btn-icon btn-strip"><i class="icon icon-code icon-sm"></i></button>';
+    const newBoxID = '';
+    const oldBoxID = '';
+    $('blockquote.textdiff').each(function() {
+        newBoxID = $(this).parent().attr('id');
+        if(newBoxID != oldBoxID) {
+            oldBoxID = newBoxID;
+            if($(this).html() != $(this).next().html()) $(this).closest('.history-changes').before(
+                diffButton);
+        }
+    });
+});
+
+$(function() {
+    const $body = $('body', window.parent.document);
+    if($body.hasClass('hide-modal-close')) $body.removeClass('hide-modal-close');
+});
