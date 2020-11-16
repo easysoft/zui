bec8b76258a6030fb05bfc8b608bd7f58cc87ec0
Tintin <istintin@qq.com>
2020-11-16 14:28:04 +0800

    template: add footer.

--- template/src/index/js/activenav.js
+++ template/src/index/js/activenav.js
@@ -16,6 +16,7 @@ $('#tabs-container').tabs({
     forbidClose: true
   }]
 });
+let breadcrumbPath = [];
 const tabsInstance = $('#tabs-container').data('zui.tabs');
 // Click on the left navigation bar to link with the tab page
 $('#treeMenu').on('click', 'a', function () {
@@ -25,6 +26,8 @@ $('#treeMenu').on('click', 'a', function () {
   }
   const tabName = $(this).text();
   const tabId = $(this).closest('li').data('id');
+  breadcrumbPath = [];
+  setBreadcrumbPath($(this));
   if (Object.keys(tabsInstance.tabs).includes(tabName)) {
     $(`#tab-nav-item-${tabName} .tab-nav-link`).trigger('click');
     return;
@@ -48,12 +51,32 @@ $('#treeMenu').on('click', 'a', function () {
 function activeMenuItem(activeTabId) {
   $('#treeMenu li.active').removeClass('active');
   $(`#treeMenu li[data-id=${activeTabId}] a`).closest('li').addClass('active');
+  breadcrumbPath = [];
+  setBreadcrumbPath($(`#treeMenu li[data-id=${activeTabId}] a`));
   const hasLists = $('#treeMenu li.active').parents('li.has-list');
   if(hasLists.length !== 0) {
     hasLists.each(function() {
       $(this).hasClass('open') || $(this).addClass('open');
     });
   }
+}
+
+/**
+ * set breadcrumb
+ * @param {JQuery<HTMLElement>} $a
+ */
+function setBreadcrumbPath($a) {
+  let $lis = $a.parents('li');
+  $lis.each(function(index, li) {
+    breadcrumbPath.unshift($(li).children('a').text());
+  });
+  const $breadcrumb = $('#footer .breadcrumb');
+  $breadcrumb.empty();
+  const breadcrumbItem = [];
+  breadcrumbPath.forEach((x, index) => {
+    breadcrumbItem.push($(`<li class="active">${x}</li>`));
+  });
+  $breadcrumb.append($('<li class="active">Z-Master</li>'), ...breadcrumbItem);
 }
 
 // Click on the tab page to link with the left navigation bar
