5862d07249ff5d376c75871edcd3d226160b0b48
Hao Sun <sunhao@easycorp.ltd>
2022-06-28 07:31:57 +0000

    + add tailwindcss.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import './global.css';
 import './style.css';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
-  <h1>Hello ZUI!</h1>
+  <h1 class="text-3xl font-bold underline text-blue-500">Hello ZUI!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
