454db012ad7e7c79efe7c18583354bebeaf7cd51
Hao Sun <sunhao@easycorp.ltd>
2022-07-01 08:30:45 +0000

    * refactor project config files.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     content: [
       "./index.html",
       "./src/**/*.{vue,js,ts,jsx,tsx}",
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import minimist from 'minimist';
+
+const argv: {_: string[]} = minimist(process.argv.slice(2));
 
 export default defineConfig(() => {
-    return {};
+    return {
+    };
 });
