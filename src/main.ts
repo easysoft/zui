77e14453bf8a280b0e433e62d107dd88d9014a12
Hao Sun <sunhao@easycorp.ltd>
2022-07-01 14:26:51 +0000

    + add stylelint config.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import '@zui/base';
 import '@zui/button';
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+const app = document.querySelector<HTMLDivElement>('#app');
 
-app.innerHTML = `
-  <h1 class="text-3xl font-bold underline text-blue-500">Hello ZUI!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+if (app) {
+    app.innerHTML = `
+    <h1 class="text-3xl font-bold underline text-blue-500">Hello ZUI!</h1>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  `;
+}
