f1c25142ec909d1f2df50c1726ed625c8cfd02ae
Hao Sun <sunhao@easycorp.ltd>
2022-07-13 21:01:18 +0800

    * update base lib.

--- lib/base/src/main.ts
+++ lib/base/src/main.ts
@@ -1,1 +1,5 @@
-import './fonts.css';
+import './modifier/solid.css';
+import './modifier/pale.css';
+import './modifier/outline.css';
+import './modifier/border.css';
+import './modifier/text.css';
