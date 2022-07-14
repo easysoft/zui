1df82ee7763b03fd9ed601a6939e243a77099cbc
Hao Sun <sunhao@easycorp.ltd>
2022-07-14 11:06:39 +0800

    * change tailwind config.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
-const {tailwind} = require('@zui/config');
+const {tailwind: zuiTailwindConfig} = require('@zui/config');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    content: [
+        './index.html',
+        './src/**/*.{vue,js,ts,jsx,tsx}',
+        './lib/*/index.html',
+        './lib/*/src/**/*.{vue,js,ts,jsx,tsx}',
+    ],
     presets: [
-        tailwind,
+        zuiTailwindConfig,
     ],
     plugins: [],
 };
