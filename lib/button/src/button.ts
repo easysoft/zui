07753d4d8df6611477eb3b61d11df79e8cb0dc57
wangzemei <wangzemei@easycorp.ltd>
2022-07-22 02:04:17 +0000

    * update webBtn

--- lib/button/src/button.ts
+++ lib/button/src/button.ts
@@ -1,68 +1,30 @@
-// import btnClass from './btn.css';
+import './vars.css';
+import './btn.css';
 
 export class ZuiButton extends HTMLElement {
-    shadowRoot: ShadowRoot;
-
     $button: any;
-
-    $loading: any;
-
-    $buttonInner: any;
 
     constructor() {
         super();
-        const template = document.createElement('template');
-        template.innerHTML = `
-            <style>
-                .btn {
-                    @apply px-3 h-8 inline-flex items-center justify-center;
-                    border: 1px solid var(--btn-border-color);
-                    background: var(--btn-background);
-                    border-radius: var(--btn-radius);
-                }
-                .btn.-xs {
-                    @apply text-xs px-1 h-5;
-                }
-                .btn.-sm {
-                    @apply text-sm px-2 h-6;
-                }
-                .btn.-lg {
-                    @apply text-lg px-4 h-10;
-                }
-                .btn.-xl {
-                    @apply text-xl px-5 h-12;
-                }
-
-                .btn.-square {
-                    @apply px-0 aspect-square;
-                }
-            </style>
-            <button class='btn'>
-                <span class='inner'><slot></slot></span>
-            </button>
-        `;
-        this.shadowRoot = this.attachShadow({mode: 'open'});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-        /* const node = document.createElement('style');
-        node.innerHTML = '@import "./btn.css";';
-        this.shadowRoot.append(node);
-        const sheet = new CSSStyleSheet();
-        sheet.replace(node.innerHTML);
-        this.shadowRoot.adoptedStyleSheets = [sheet]; */
-        /* 3 fetch('./btn.css').then(response => response.text()).then(data => {
-            const node2 = document.createElement('style');
-            node2.innerHTML = data;
-            this.shadowRoot.appendChild(node2);
-        }); */
-        this.$button = this.shadowRoot.querySelector('button');
-        this.$loading = this.shadowRoot.querySelector('loading');
-        this.$buttonInner = this.shadowRoot.querySelector('.inner');
+      
+        this.$button = document.createElement('button');
+        const text:string = this.innerHTML;
+        this.innerHTML = '';
+        this.$button.innerHTML = text;
+        this.$button.classList.add('btn');
+        this.append(this.$button);
         this.$button.addEventListener('click', () => this.onClick());
     }
 
     connectedCallback() {
-        if (this.type && this.$button) {
-            this.$button.setAttribute('class', `${this.class} -${this.type}`);
+        if (this.$button && this.type) {
+            this.$button.classList.add(`-${this.type}`);
+        }
+        if (this.$button && this.size) {
+            this.$button.classList.add(`-${this.size}`);
+        }
+        if (this.$button && this.rounded) {
+            this.$button.classList.add(`-rounded-${this.rounded}`);
         }
     }
 
@@ -71,10 +33,10 @@ export class ZuiButton extends HTMLElement {
     }
 
     set type(value) {
-        if (this.getAttribute('type') === null) {
+        if (!this.getAttribute('type')) {
             this.removeAttribute('type');
         } else {
-            this.setAttribute('type', value || '');
+            this.setAttribute('type', value);
         }
     }
 
@@ -83,7 +45,7 @@ export class ZuiButton extends HTMLElement {
     }
 
     set size(value) {
-        if (this.getAttribute('size') === null) {
+        if (!this.getAttribute('size')) {
             this.removeAttribute('size');
         } else {
             this.setAttribute('size', value || 'base');
@@ -144,9 +106,7 @@ export class ZuiButton extends HTMLElement {
     }
 
     render(name: string) {
-        if (name === 'rounded') {
-            this.$button.classList.push('-rounded-full');
-        }
+        console.log(name);
     }
 }
 
