4891f3a63a9ee6e1de669c1fce64692e56aa9e57
Hao Sun <sunhao@easycorp.ltd>
2022-07-26 16:26:16 +0800

    * support for watching lib docs in docs dev mode.

--- tools/prepare-docs.ts
+++ tools/prepare-docs.ts
@@ -1,7 +1,10 @@
 import path from 'path';
 import fs from 'fs-extra';
+import minimist from 'minimist';
 import {getBuildInLibs} from './buildin-libs.js';
 import {exec} from './exec.js';
+
+const argv = minimist(process.argv.slice(3));
 
 await fs.emptyDir(path.resolve(process.cwd(), './docs/assets'));
 
@@ -14,13 +17,15 @@ const buildInLibs = await getBuildInLibs({readNameFromDoc: true});
 await fs.outputJSON(path.resolve(process.cwd(), './docs/assets/zui-lib.json'), Array.from(buildInLibs.values()), {spaces: 4});
 await fs.outputFile(path.resolve(process.cwd(), './docs/assets/zui-lib.js'), `export default ${JSON.stringify(Array.from(buildInLibs.values()), null, 4)};`);
 
-for (const lib of buildInLibs.values()) {
-    const originLibDocsDir = path.resolve(process.cwd(), './lib/', lib.shortName, 'docs');
-    const originLibDocsDirExists = await fs.pathExists(originLibDocsDir);
-    if (!originLibDocsDirExists) {
-        continue;
-    }
+if (argv.copy) {
+    for (const lib of buildInLibs.values()) {
+        const originLibDocsDir = path.resolve(process.cwd(), './lib/', lib.shortName, 'docs');
+        const originLibDocsDirExists = await fs.pathExists(originLibDocsDir);
+        if (!originLibDocsDirExists) {
+            continue;
+        }
 
-    const libDocsDir = path.resolve(process.cwd(), './docs/lib', lib.shortName);
-    await fs.copy(originLibDocsDir, libDocsDir);
+        const libDocsDir = path.resolve(process.cwd(), './docs/lib', lib.shortName);
+        await fs.copy(originLibDocsDir, libDocsDir);
+    }
 }
---
+++ tools/watch-docs.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import fs from 'fs-extra';
+import chokidar from 'chokidar';
+
+const libPath = path.resolve(process.cwd(), './lib/');
+const docsPath = path.resolve(process.cwd(), './docs/lib/');
+const watchPath = `${libPath}/*/docs/**/*.md`;
+const watcher = chokidar.watch(watchPath, {persistent: true, ignoreInitial: false});
+
+console.log(`> Watch docs... ${watchPath}`);
+
+function getDestFile(file: string) {
+    const libName = file.replace(`${libPath}/`, '').split('/')[0];
+    return path.resolve(docsPath, libName, file.replace(`${libPath}/${libName}/docs/`, ''));
+}
+watcher.on('add', (file) => {
+    const destFile = getDestFile(file);
+    fs.ensureDirSync(path.dirname(destFile));
+    fs.copyFileSync(file, destFile);
+    console.log(`> ADD:    ${path.relative(docsPath, destFile)}`);
+});
+watcher.on('change', (file) => {
+    const destFile = getDestFile(file);
+    fs.copyFileSync(file, destFile);
+    console.log(`> CHANGE: ${path.relative(docsPath, destFile)}`);
+});
+watcher.on('unlink', (file) => {
+    const destFile = getDestFile(file);
+    fs.removeSync(destFile);
+    console.log(`> REMOVE: ${path.relative(docsPath, destFile)}`);
+});
