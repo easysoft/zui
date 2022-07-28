349cd87ae0281458f6746b52b55bce6aba2cab9d
Hao Sun <sunhao@easycorp.ltd>
2022-07-28 22:21:07 +0800

    - remove unused files.

--- .vuepress/client.ts
+++
@@ -1,7 +0,0 @@
-import { defineClientConfig } from '@vuepress/client'
-
-export default defineClientConfig({
-    enhance({ app, router, siteData }) {},
-    setup() {},
-    rootComponents: [],
-});
