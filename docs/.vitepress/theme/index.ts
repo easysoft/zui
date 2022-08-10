2fac61282f6e7305f0fb54f2ac7ae705db97dab5
Hao Sun <sunhao@easycorp.ltd>
2022-08-10 13:05:20 +0800

    * docs: fix build not work.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,8 @@
 import DefaultTheme from './theme-default';
-import * as zui from '../../assets/zui/zui.js';
-import '../../assets/zui/zui.css';
 import './tailwind.css';
 import './vars.css';
 import './style.css';
 import Example from './components/example.vue';
-
-Object.assign(window, {zui});
 
 export default {
     ...DefaultTheme,
