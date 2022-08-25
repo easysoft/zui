0cd1b97fd318597aa03dee067868d2404f398622
Hao Sun <sunhao@easycorp.ltd>
2022-08-25 17:14:24 +0800

    * docs: fix tailwind utilities not work.

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -3,8 +3,8 @@ const {tailwind: zuiTailwindConfig} = require('zui-config');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './**/*.{vue,js,ts,jsx,tsx,md}',
-        './.vitepress/**/*.{vue,js,ts,jsx,tsx,md}',
+        './_/**/*.{vue,js,ts,jsx,tsx,md}',
+        './_/.vitepress/**/*.{vue,js,ts,jsx,tsx,md}',
         '!node_modules',
         '!public',
     ],
