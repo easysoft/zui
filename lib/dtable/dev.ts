82bb1111c9d9488abb678efb0de58caa13e35340
Hao Sun <sunhao@easycorp.ltd>
2022-09-02 19:34:09 +0800

    * dtable: refactor dev example.

--- lib/dtable/dev.ts
+++ lib/dtable/dev.ts
@@ -6,10 +6,71 @@ import {DTable} from './src/main';
 import checkable from './src/plugins/checkable';
 import nested from './src/plugins/nested';
 import rich from './src/plugins/rich';
+import headerGroup from './src/plugins/header-group';
 
 faker.setLocale('zh_CN');
 
 setTimeout(() => {
+    const productElement = document.getElementById('productTable');
+    if (productElement) {
+        const productTable = new DTable(productElement, {
+            cols: [
+                {name: 'name', title: '产品名称', type: 'link', width: 280, fixed: 'left', sortType: 'asc', linkTemplate: '#/product/{id}'},
+                {name: 'productLine', title: '所属产品线', minWidth: 110, sortType: true, flex: 1},
+                {name: 'manager', title: '负责人', type: 'avatar', width: 110, sortType: true, avatarKey: 'managerAvatar', avatarWithName: true},
+                {name: 'feedback', title: '反馈', width: 65, sortType: true, align: 'center'},
+                {name: 'storyDraft', title: '需求草稿', width: 90, sortType: true, align: 'center', group: '需求情况'},
+                {name: 'storyActive', title: '激活', width: 80, sortType: true, align: 'center', group: '需求情况'},
+                {name: 'storyChanged', title: '变更', width: 80, sortType: true, align: 'center', group: '需求情况'},
+                {name: 'completion', title: '完成率', type: 'circleProgress', width: 80, sortType: true, align: 'center', group: '需求情况'},
+                {name: 'plan', title: '计划', width: 80, sortType: true, align: 'center'},
+                {name: 'caseCoverage', title: '用例覆盖率', type: 'circleProgress', width: 90, sortType: true, align: 'center'},
+                {name: 'bugActive', title: 'Bug激活', group: 'Bug情况', width: 80, sortType: true, align: 'center'},
+                {name: 'fixRate', title: '修复率', group: 'Bug情况', type: 'circleProgress', width: 80, sortType: true, align: 'center'},
+                {name: 'release', title: '发布', width: 90, sortType: true, fixed: 'right', onRenderCell(result, rowID, col, data) {
+                    const releaseIncrease = Number(data?.releaseIncrease);
+                    result[0] = {
+                        html: `<strong>${result[0]}</strong>`,
+                    };
+                    if (releaseIncrease > 6) {
+                        result.push({
+                            html: `<span class="label size-sm ${data?.milestone ? 'important' : 'secondary'}-pale circle">+${releaseIncrease - 6}</span>`,
+                        });
+                    }
+                    return result;
+                }},
+            ],
+            data: Array(20).fill(0).map((_, index) => ({
+                id: `${index}`,
+                name: faker.animal.cetacean(),
+                productLine: faker.lorem.word(),
+                manager: `${faker.name.lastName()}${faker.name.firstName()}`,
+                feedback: faker.datatype.number({min: 0, max: 150}),
+                storyDraft: faker.datatype.number({min: 0, max: 150}),
+                storyActive: faker.datatype.number({min: 0, max: 150}),
+                storyChanged: faker.datatype.number({min: 0, max: 150}),
+                completion: faker.datatype.number({min: 0, max: 100}),
+                plan: faker.datatype.number({min: 0, max: 10}),
+                execution: faker.datatype.number({min: 0, max: 20}),
+                caseCoverage: faker.datatype.number({min: 0, max: 100}),
+                bugActive: faker.datatype.number({min: 0, max: 100}),
+                fixRate: faker.datatype.number({min: 0, max: 100}),
+                release: faker.datatype.number({min: 0, max: 200}),
+                releaseIncrease: faker.datatype.number({min: 0, max: 10}),
+                milestone: faker.datatype.boolean(),
+                managerAvatar: `/lib/avatar/assets/avatar-${faker.datatype.number({min: 1, max: 10})}.png`,
+            })),
+            height: 400,
+            striped: false,
+            bordered: true,
+            plugins: [rich, headerGroup],
+            responsive: true,
+        });
+        console.log('productTable', productTable);
+    } else {
+        console.log('DataTable element not found #productElement');
+    }
+
     const element = document.getElementById('datatableExample');
     if (element) {
         const datatable = new DTable(element, {
@@ -54,72 +115,4 @@ setTimeout(() => {
     } else {
         console.log('DataTable element not found #datatableExample');
     }
-
-    const productElement = document.getElementById('productTable');
-    if (productElement) {
-        const productTable = new DTable(productElement, {
-            cols: [
-                {name: 'name', title: '产品名称', type: 'link', width: 280, fixed: 'left', sortType: 'asc', linkTemplate: '#/product/{id}'},
-                {name: 'productLine', title: '所属产品线', minWidth: 110, sortType: true, flex: 1},
-                {name: 'manager', title: '负责人', type: 'avatar', width: 110, sortType: true, avatarKey: 'managerAvatar', avatarWithName: true},
-                {name: 'feedback', title: '反馈', width: 65, sortType: true, align: 'center'},
-                {name: 'storyDraft', title: '需求草稿', width: 90, sortType: true, align: 'center'},
-                {name: 'storyActive', title: '激活', width: 80, sortType: true, align: 'center'},
-                {name: 'storyChanged', title: '变更', width: 80, sortType: true, align: 'center'},
-                {name: 'completion', title: '完成率', type: 'circleProgress', width: 80, sortType: true, align: 'center'},
-                {name: 'plan', title: '计划', width: 80, sortType: true, align: 'center'},
-                {name: 'caseCoverage', title: '用例覆盖率', type: 'circleProgress', width: 90, sortType: true, align: 'center'},
-                {name: 'bugActive', title: 'Bug激活', width: 80, sortType: true, align: 'center'},
-                {name: 'fixRate', title: '修复率', type: 'circleProgress', width: 80, sortType: true, align: 'center'},
-                {name: 'release', title: '发布', width: 90, sortType: true, fixed: 'right', onRenderCell(result, rowID, col, data) {
-                    const releaseIncrease = Number(data?.releaseIncrease);
-                    result[0] = {
-                        html: `<strong>${result[0]}</strong>`,
-                    };
-                    if (releaseIncrease > 6) {
-                        result.push({
-                            html: `<span class="label size-sm ${data?.milestone ? 'important' : 'secondary'}-pale circle">+${releaseIncrease - 6}</span>`,
-                        });
-                    }
-                    return result;
-                }},
-            ],
-            data: Array(20).fill(0).map((_, index) => ({
-                id: `${index}`,
-                name: faker.animal.cetacean(),
-                productLine: faker.lorem.word(),
-                manager: `${faker.name.lastName()}${faker.name.firstName()}`,
-                feedback: faker.datatype.number({min: 0, max: 150}),
-                storyDraft: faker.datatype.number({min: 0, max: 150}),
-                storyActive: faker.datatype.number({min: 0, max: 150}),
-                storyChanged: faker.datatype.number({min: 0, max: 150}),
-                completion: faker.datatype.number({min: 0, max: 100}),
-                plan: faker.datatype.number({min: 0, max: 10}),
-                execution: faker.datatype.number({min: 0, max: 20}),
-                caseCoverage: faker.datatype.number({min: 0, max: 100}),
-                bugActive: faker.datatype.number({min: 0, max: 100}),
-                fixRate: faker.datatype.number({min: 0, max: 100}),
-                release: faker.datatype.number({min: 0, max: 200}),
-                releaseIncrease: faker.datatype.number({min: 0, max: 10}),
-                milestone: faker.datatype.boolean(),
-                managerAvatar: `/lib/avatar/assets/avatar-${faker.datatype.number({min: 1, max: 10})}.png`,
-                // manager: '张三',
-                // storyScale: 451,
-                // executionCount: 451,
-                // invested: 451,
-                // startDate: '2020-01-01',
-                // endDate: '2020-01-01',
-                // progress: '50%',
-                // parent: `${['', '', '', index - 1, index - 2, index - 3, index - 3, index - 4, index - 1, index - 1][index % 10]}`,
-            })),
-            height: 400,
-            striped: false,
-            // bordered: true,
-            plugins: [rich],
-            responsive: true,
-        });
-        console.log('productTable', productTable);
-    } else {
-        console.log('DataTable element not found #productElement');
-    }
-}, 1000);
+}, 1500);
