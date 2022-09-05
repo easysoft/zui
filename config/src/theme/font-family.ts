f14190a4a8b1a6b9c39ef2655effe6144ef1c338
Hao Sun <sunhao@easycorp.ltd>
2022-09-05 12:12:36 +0800

    * config: add font family config.

---
+++ config/src/theme/font-family.ts
@@ -0,0 +1,4 @@
+module.exports = {
+    sans: ['-apple-system', 'Noto Sans', 'Helvetica Neue', 'Helvetica', 'Nimbus Sans L', 'Arial', 'Liberation Sans', 'PingFang SC', 'Hiragino Sans GB', 'Noto Sans CJK SC', 'Source Han Sans SC', 'Source Han Sans CN', 'Microsoft YaHei', 'Wenquanyi Micro Hei', 'WenQuanYi Zen Hei', 'ST Heiti', 'SimHei', 'WenQuanYi Zen Hei Sharp', 'sans-serif'],
+    serif: ['Nimbus Roman No9 L', 'Songti SC', 'Noto Serif CJK SC', 'Source Han Serif SC', 'Source Han Serif CN', 'STSong', 'AR PL New Sung', 'AR PL SungtiL GB', 'NSimSun', 'SimSun', 'TW-Sung', 'WenQuanYi Bitmap Song', 'AR PL UMing CN', 'AR PL UMing HK', 'AR PL UMing TW', 'AR PL UMing TW MBE', 'PMingLiU', 'MingLiU', 'serif'],
+};
