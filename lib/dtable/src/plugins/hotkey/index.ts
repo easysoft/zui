537cd4e9cf55ace08171b3b2d9f1cea0efeac25a
Hao Sun <sunhao@easycorp.ltd>
2022-09-22 09:05:45 +0800

    * dtable: fix hotkey not work.

--- lib/dtable/src/plugins/hotkey/index.ts
+++ lib/dtable/src/plugins/hotkey/index.ts
@@ -1,27 +1,39 @@
 import hotkeys, {HotkeysEvent} from 'hotkeys-js';
 import {definePlugin} from '../../helpers/shared-plugins';
-import './style.css';
 
-type DTableHotkeyCallback = (this: DTableHotkey, event: KeyboardEvent, handler: HotkeysEvent) => void;
+export type DTableHotkeyCallback = (this: DTable, event: KeyboardEvent, handler: HotkeysEvent) => void;
 
-type DTableHotkeyTypes = {
+export type DTableHotkeyTypes = {
     options: Partial<{
         hotkeys: Record<string, DTableHotkeyCallback>;
     }>,
     data: {
         hotkeys: Map<string, DTableHotkeyCallback>;
         keys?: string;
+    },
+    methods: {
+        hotkeyHandler: (this: DTableHotkey, event: KeyboardEvent, handler: HotkeysEvent) => void;
     }
 };
 
 type DTableHotkey = DTableWithPlugin<DTableHotkeyTypes>;
 
 export const headerGroup: DTablePlugin<DTableHotkeyTypes> = {
-    name: 'header-group',
+    name: 'hotkey',
     data: {
         hotkeys: new Map(),
     },
     when: options => !!options.hotkeys,
+    methods: {
+        hotkeyHandler(event, handler) {
+            this.data.hotkeys.get(handler.key)?.call(this, event, handler);
+        },
+    },
+    events: {
+        click() {
+            hotkeys.setScope(this.id);
+        },
+    },
     onMounted() {
         const {hotkeys: hotkeysOptions} = this.options;
         if (!hotkeysOptions) {
@@ -44,18 +56,12 @@ export const headerGroup: DTablePlugin<DTableHotkeyTypes> = {
             return;
         }
         this.data.keys = keys.join(',');
-        hotkeys(this.data.keys, {
-            element: this.ref.current,
-            scope: this.id,
-        }, (event, handler) => {
-            hotkeysMap.get(handler.key)?.call(this, event, handler);
-        });
-
+        hotkeys(this.data.keys, {scope: this.id}, this.hotkeyHandler);
         this.data.hotkeys = hotkeysMap;
     },
     onUnmounted() {
         if (this.data.keys) {
-            hotkeys.unbind(this.data.keys, this.id);
+            hotkeys.unbind(this.data.keys, this.id, this.hotkeyHandler);
         }
     },
 };
