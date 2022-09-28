e1db574e49d017d392c8259832e7c53ed46cf1b8
Hao Sun <sunhao@easycorp.ltd>
2022-09-28 11:23:06 +0800

    * dtable: add new plugin "store".

---
+++ lib/dtable/src/plugins/store/index.ts
@@ -0,0 +1,28 @@
+import {Store} from '@zui/store/src/store';
+import {definePlugin} from '../../helpers/shared-plugins';
+
+export interface DTableStoreTypes extends DTablePluginTypes {
+    options: Partial<{
+        store: boolean;
+    }>;
+    data: {
+        store: Store;
+    },
+}
+
+export type DTableStore = DTableWithPlugin<DTableStoreTypes>;
+
+export const store: DTablePlugin<DTableStoreTypes> = {
+    name: 'store',
+    defaultOptions: {
+        store: true,
+    },
+    when: options => !!options.store,
+    data() {
+        return {store: new Store(this.id)};
+    },
+};
+
+const plugin = definePlugin(store);
+
+export default plugin;
