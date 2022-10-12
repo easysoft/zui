240fd3eec7d8d97597715f99e31722d8b647dc59
Hao Sun <sunhao@easycorp.ltd>
2022-10-12 18:12:30 +0800

    * com-helpers: refactor react component helper.

--- lib/com-helpers/src/helpers/component-react.tsx
+++ lib/com-helpers/src/helpers/component-react.tsx
@@ -1,33 +1,38 @@
-import {render} from 'preact';
-import type {ComponentType} from 'preact';
+import {createRef, render} from 'preact';
+import type {ComponentType, Component} from 'preact';
 import {ComponentBase} from './vanilla-component';
 
-export declare class ComponentReactClass<O extends object = {}, E extends HTMLElement = HTMLElement> {
+export declare class ComponentReactClass<O extends object = {}, C extends Component<O> = Component<O>, E extends HTMLElement = HTMLElement> {
     static NAME: string;
+
+    static Component: ComponentType;
+
+    $: C | null;
 
     options: O;
 
     element: E;
-
-    Component: ComponentType;
 
     constructor(element: E | string, options: O);
 
     render(options: Partial<O>): void;
 }
 
-export abstract class ComponentFromReact<O extends object = {}, E extends HTMLElement = HTMLElement> extends ComponentBase<O, E> implements ComponentReactClass {
-    static NAME: string;
+export abstract class ComponentFromReact<O extends object, C extends Component<O>, E extends HTMLElement = HTMLElement> extends ComponentBase<O, E> implements ComponentReactClass<O, C, E> {
+    #ref = createRef<C>();
 
-    abstract Component: ComponentType;
+    get $() {
+        return this.#ref.current;
+    }
 
     init() {
         requestAnimationFrame(() => this.render());
     }
 
     render(options?: Partial<O>) {
+        const Component = (this.constructor as typeof ComponentReactClass<O, C, E>).Component;
         render((
-            <this.Component {...this.setOptions(options)} />
+            <Component ref={this.#ref} {...this.setOptions(options)} />
         ), this.element);
     }
 }
