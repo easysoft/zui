591e4e1b810942a615435396afe5ac20ab90095f
Hao Sun <sunhao@easycorp.ltd>
2022-10-14 10:37:55 +0800

    * dtable: add new plugin "contextmenu"

---
+++ lib/dtable/src/plugins/contextmenu/index.ts
@@ -0,0 +1,56 @@
+import {ContextMenu} from '@zui/contextmenu';
+import type {MenuListItem} from '@zui/menu/src/component/menu';
+import {definePlugin} from '../../helpers/shared-plugins';
+
+export interface DTableContextMenuTypes extends DTablePluginTypes {
+    options: Partial<{
+        contextmenu: Partial<{
+            cell: MenuListItem[] | ((event: MouseEvent, info: DTablePointerInfo, menu: ContextMenu) => MenuListItem[] | undefined),
+            header: MenuListItem[] | ((event: MouseEvent, info: DTablePointerInfo, menu: ContextMenu) => MenuListItem[] | undefined),
+        }>;
+    }>;
+    data: {
+        contextmenu: ContextMenu
+    },
+}
+
+export type DTableContextMenu = DTableWithPlugin<DTableContextMenuTypes>;
+
+export const contextmenu: DTablePlugin<DTableContextMenuTypes> = {
+    name: 'contextmenu',
+    defaultOptions: {
+        contextmenu: {},
+    },
+    when: options => !!options.contextmenu,
+    onMounted() {
+        const {current} = this.ref;
+        if (current) {
+            this.data.contextmenu = new ContextMenu(current, {
+                items: (menu, event) => {
+                    const info = this.getPointerInfo(event);
+                    if (!info) {
+                        return;
+                    }
+                    const {contextmenu: {header, cell} = {}} = this.options;
+                    if (info.rowID === 'HEADER') {
+                        if (typeof header === 'function') {
+                            return header(event, info, menu);
+                        }
+                        return header;
+                    }
+                    if (typeof cell === 'function') {
+                        return cell(event, info, menu);
+                    }
+                    return cell;
+                },
+            });
+        }
+    },
+    onUnmounted() {
+        this.data.contextmenu?.destroy();
+    },
+};
+
+const plugin = definePlugin(contextmenu);
+
+export default plugin;
