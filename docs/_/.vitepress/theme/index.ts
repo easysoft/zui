60a041c35b043d6f195838912faa7e53b8007165
Hao Sun <sunhao@easycorp.ltd>
2022-10-19 16:31:31 +0800

    * docs: add onZUIReady helper to global.

--- docs/_/.vitepress/theme/index.ts
+++ docs/_/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import DefaultTheme from './theme-default';
 import './tailwind.css';
 import './vars.css';
 import './style.css';
+import {onZUIReady} from './zui-ready';
 import Example from './components/example.vue';
 
 export default {
@@ -10,3 +11,5 @@ export default {
         app.component('Example', Example);
     }
 }
+
+Object.assign(window, {onZUIReady});
