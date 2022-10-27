ecbc59cc9d945c093ceadc46a913a23e8830bc9d
Hao Sun <sunhao@easycorp.ltd>
2022-10-27 16:56:45 +0800

    * menu: fix example in dev page.

--- lib/menu/dev.ts
+++ lib/menu/dev.ts
@@ -7,13 +7,13 @@ import {Menu} from './src/main';
 onPageLoad(() => {
     const menu = new Menu('#menu', {
         items: [
-            {title: '复制', icon: 'icon-copy'},
-            {title: '粘贴', icon: 'icon-paste'},
-            {title: '剪切'},
-            {type: 'heading', title: '更多操作'},
-            {title: '导入', icon: 'icon-upload-alt'},
-            {title: '导出', icon: 'icon-download-alt'},
-            {title: '保存', icon: 'icon-save', onClick: (event) => console.log('> menuItem.clicked', event)},
+            {text: '复制', icon: 'icon-copy'},
+            {text: '粘贴', icon: 'icon-paste'},
+            {text: '剪切'},
+            {type: 'heading', text: '更多操作'},
+            {text: '导入', icon: 'icon-upload-alt'},
+            {text: '导出', icon: 'icon-download-alt'},
+            {text: '保存', icon: 'icon-save', onClick: (event) => console.log('> menuItem.clicked', event)},
         ],
         onClickItem: (info) => {
             console.log('> menu.onClickItem', info);
