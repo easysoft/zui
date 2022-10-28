12cdf0083cadddc938e748b6bbd86577863e6ca4
Hao Sun <sunhao@easycorp.ltd>
2022-10-28 11:12:31 +0800

    * menu: fix type issue.

--- lib/menu/src/component/menu.tsx
+++ lib/menu/src/component/menu.tsx
@@ -1,19 +1,19 @@
 import {ActionMenu} from '@zui/action-menu/src/component/action-menu';
 import {MenuOptions} from '../types/menu-options';
 import {MenuItemOptions} from '../types/menu-item-options';
-import {MenuItemOptions} from '../types/menu-item-props';
 import '@zui/css-icons/src/icons/caret.css';
 import '../style/index.css';
-import {ActionMenuItem} from '@zui/action-menu/src/types/action-menu-item';
+import {ActionMenuItemOptions} from '@zui/action-menu/src/types/action-menu-item-options';
+import {ActionItemProps} from '@zui/action-menu/src/types/action-item-props';
 import {classes} from '@zui/browser-helpers/src/classes';
 
-export class Menu<T extends ActionMenuItem = MenuItemOptions> extends ActionMenu<T, MenuOptions<T>> {
+export class Menu<T extends ActionMenuItemOptions = MenuItemOptions> extends ActionMenu<T, MenuOptions<T>> {
     beforeRender() {
         const options = super.beforeRender();
         let {hasIcons} = options;
 
         if (hasIcons === undefined) {
-            hasIcons = options.items.some(x => 'icon' in x && (x as MenuItemOptions).icon);
+            hasIcons = options.items.some(x => (x as ActionItemProps).icon);
         }
         if (hasIcons) {
             options.className = classes(options.className, 'has-icons');
