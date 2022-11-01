de1d74ac076dd2913a963127ba4acf59698a2c01
Hao Sun <sunhao@easycorp.ltd>
2022-11-01 19:52:02 +0800

    * action-menu: refactor.

--- lib/action-menu/src/types/index.ts
+++ lib/action-menu/src/types/index.ts
@@ -3,7 +3,6 @@ export * from './action-divider-props';
 export * from './action-heading-props';
 export * from './action-item-props';
 export * from './action-nested-item-props';
-export * from './action-dropdown-props';
 export * from './action-space-props';
 export * from './action-menu-item-options';
 export * from './action-menu-options';
