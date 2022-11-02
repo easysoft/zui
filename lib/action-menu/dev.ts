c138f138caa0fa24cf6f1346b54dc9145f610c11
Hao Sun <sunhao@easycorp.ltd>
2022-11-02 17:27:32 +0800

    * action-menu: refactor example in dev page.

--- lib/action-menu/dev.ts
+++ lib/action-menu/dev.ts
@@ -7,21 +7,21 @@ import {ActionMenu, ActionMenuNested} from './src/main';
 import {ActionMenuItemOptions, ActionMenuNestedItemOptions} from './src/types';
 
 onPageUpdate(() => {
-    // const menu = new ActionMenu<ActionMenuItemOptions>('#actionMenu', {
-    //     items: [
-    //         {text: '复制', icon: 'icon-copy'},
-    //         {text: '粘贴', icon: 'icon-paste'},
-    //         {text: '剪切'},
-    //         {type: 'divider'},
-    //         {type: 'heading', text: '更多操作'},
-    //         {text: '导入', icon: 'icon-upload-alt'},
-    //         {text: '导出', icon: 'icon-download-alt'},
-    //     ],
-    //     onClickItem: (info) => {
-    //         console.log('> menu.onClickItem', info);
-    //     },
-    // });
-    // console.log('> menu', menu);
+    const menu = new ActionMenu<ActionMenuItemOptions>('#actionMenu', {
+        items: [
+            {text: '复制', icon: 'icon-copy'},
+            {text: '粘贴', icon: 'icon-paste'},
+            {text: '剪切'},
+            {type: 'divider'},
+            {type: 'heading', text: '更多操作'},
+            {text: '导入', icon: 'icon-upload-alt'},
+            {text: '导出', icon: 'icon-download-alt'},
+        ],
+        onClickItem: (info) => {
+            console.log('> menu.onClickItem', info);
+        },
+    });
+    console.log('> menu', menu);
 
     const nestedMenu = new ActionMenuNested<ActionMenuNestedItemOptions>('#nestedActionMenu', {
         nestedTrigger: 'click',
@@ -50,35 +50,35 @@ onPageUpdate(() => {
     });
     console.log('> nestedMenu', nestedMenu);
 
-    // const nav = new ActionMenu('#nav', {
-    //     name: 'nav',
-    //     items: [
-    //         {text: '首页', icon: 'icon-home'},
-    //         {text: '动态'},
-    //         {text: '论坛'},
-    //         {type: 'divier'},
-    //         {text: '博客', icon: 'icon-rss'},
-    //         {text: '关注我们', icon: 'icon-user-group'},
-    //     ],
-    //     onClickItem: (info) => {
-    //         console.log('> nav.onClickItem', info);
-    //     },
-    // });
-    // console.log('> nav', nav);
+    const nav = new ActionMenu('#nav', {
+        name: 'nav',
+        items: [
+            {text: '首页', icon: 'icon-home'},
+            {text: '动态'},
+            {text: '论坛'},
+            {type: 'divier'},
+            {text: '博客', icon: 'icon-rss'},
+            {text: '关注我们', icon: 'icon-user-group'},
+        ],
+        onClickItem: (info) => {
+            console.log('> nav.onClickItem', info);
+        },
+    });
+    console.log('> nav', nav);
 
-    // const navExample = new ActionMenu<ActionMenuItemOptions>('#navExample', {
-    //     name: 'nav',
-    //     items: [
-    //         {text: '首页', icon: 'icon-home'},
-    //         {text: '动态'},
-    //         {text: '论坛'},
-    //         {type: 'divier'},
-    //         {text: '博客', icon: 'icon-rss'},
-    //         {text: '关注我们', icon: 'icon-user-group'},
-    //     ],
-    //     onClickItem: (info) => {
-    //         console.log('> nav.onClickItem', info);
-    //     },
-    // });
-    // console.log('> navExample', navExample);
+    const navExample = new ActionMenu<ActionMenuItemOptions>('#navExample', {
+        name: 'nav',
+        items: [
+            {text: '首页', icon: 'icon-home'},
+            {text: '动态'},
+            {text: '论坛'},
+            {type: 'divier'},
+            {text: '博客', icon: 'icon-rss'},
+            {text: '关注我们', icon: 'icon-user-group'},
+        ],
+        onClickItem: (info) => {
+            console.log('> nav.onClickItem', info);
+        },
+    });
+    console.log('> navExample', navExample);
 });
