ec8d6ad62c056e35609ee2f22a8a583caccdd670
Hao Sun <sunhao@easycorp.ltd>
2022-11-07 16:56:40 +0800

    + dtable: add new plugin "toolbar".

---
+++ lib/dtable/src/plugins/toolbar/index.tsx
@@ -0,0 +1,28 @@
+import {Toolbar} from '@zui/toolbar/src/component/toolbar';
+import type {ToolbarOptions} from '@zui/toolbar/src/types';
+import {definePlugin} from '../../helpers/shared-plugins';
+import type {DTablePlugin} from '../../types/plugin';
+
+
+type DTableToolbarTypes = {
+    options: Partial<{
+        footToolbar: ToolbarOptions
+    }>
+};
+
+/**
+ * @todo auto calculate column width by toolbar setting
+ */
+export const toolbarPlugin: DTablePlugin<DTableToolbarTypes> = {
+    name: 'toolbar',
+    footer: {
+        toolbar() {
+            const {footToolbar} = this.options;
+            return [footToolbar ? <Toolbar {...footToolbar} /> : null];
+        },
+    },
+};
+
+export const toolbar = definePlugin(toolbarPlugin);
+
+export default toolbar;
