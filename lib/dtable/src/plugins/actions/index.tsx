e9d667265fb484e63ff1d690f6253ffe0f03582f
Hao Sun <sunhao@easycorp.ltd>
2022-11-10 15:17:19 +0800

    * dtable: support to setting dropdown items dynamically

--- lib/dtable/src/plugins/actions/index.tsx
+++ lib/dtable/src/plugins/actions/index.tsx
@@ -1,9 +1,10 @@
 import {Toolbar} from '@zui/toolbar/src/component/toolbar';
-import type {ToolbarOptions, ToolbarItemOptions} from '@zui/toolbar/src/types';
+import type {ToolbarOptions, ToolbarItemOptions, ToolbarDropdownProps} from '@zui/toolbar/src/types';
 import {definePlugin} from '../../helpers/shared-plugins';
 import type {DTablePlugin} from '../../types/plugin';
 import type {RowInfo} from '../../types/row';
 import type {ColInfo} from '../../types/col';
+import type {MenuItemOptions} from '@zui/menu/src/types';
 
 export type DTableActionsTypes = {
     col: Partial<{
@@ -22,7 +23,7 @@ const actionsPlugin: DTablePlugin<DTableActionsTypes> = {
         actions: {
             onRenderCell(result, info) {
                 const {row, col} = info;
-                const actionItems = row.data?.[col.name] as (string | Partial<ToolbarItemOptions & {name: string}>)[];
+                const actionItems = row.data?.[col.name] as (string | Partial<ToolbarItemOptions & {name: string, items?: (string | MenuItemOptions)[]}>)[];
                 if (!actionItems) {
                     return result;
                 }
@@ -35,9 +36,23 @@ const actionsPlugin: DTablePlugin<DTableActionsTypes> = {
                         if (!action) {
                             return;
                         }
-                        const {name, ...others} = action;
+                        const {name, items, ...others} = action;
                         if (actionsMap && name) {
                             Object.assign(others, actionsMap[name]);
+                        }
+                        if (items && others.type === 'dropdown') {
+                            const {dropdown = {}} = (others as ToolbarDropdownProps);
+                            dropdown.items = items.reduce((list, item) => {
+                                const itemAction = typeof item === 'string' ? {name: item} : item;
+                                if (itemAction) {
+                                    if (actionsMap && 'name' in itemAction) {
+                                        Object.assign(itemAction, actionsMap[itemAction.name]);
+                                    }
+                                    list.push(itemAction as MenuItemOptions);
+                                }
+                                return list;
+                            }, [] as MenuItemOptions[]);
+                            (others as ToolbarDropdownProps).dropdown = dropdown;
                         }
                         return others;
                     }).filter(Boolean) as ToolbarItemOptions[],
