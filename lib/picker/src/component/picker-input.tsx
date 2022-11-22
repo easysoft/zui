e1809f3d213fab8476caa2ae57d531aa5fa7e12a
Zoe-ww <2249978581@qq.com>
2022-11-22 17:14:56 +0800

    * Format optimization

--- lib/picker/src/component/picker-input.tsx
+++ lib/picker/src/component/picker-input.tsx
@@ -4,7 +4,6 @@ import {classes} from '@zui/browser-helpers/src/classes';
 import {Attributes, ComponentType, h as _h, Component} from 'preact';
 import type {PickerOptions} from '../types/picker-options';
 
-
 export class PickerInput extends Component<PickerOptions> {
     render() {
         const {
@@ -17,12 +16,12 @@ export class PickerInput extends Component<PickerOptions> {
             {className: classes('picker')} as Attributes, 
             (
                 <div class="picker-selections">
-                <div class="input-control suffix-sm">
+                    <div class="input-control suffix-sm">
                         <input type="text" class={classes('form-control', {disabled})} name="pickerExp2" placeholder={placeholder} onInput={onInput}/>
                         <span class="input-control-suffix"><span class="caret"></span></span>
                     </div>
                 </div>
             ),
-        )
-    };
+        );
+    }
 }
