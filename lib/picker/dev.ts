dfb7aa6b326b4b8ecf9f3b9198e73a2dbb66fc2f
Zoe-ww <2249978581@qq.com>
2022-11-23 15:19:41 +0800

    * picker: css

--- lib/picker/dev.ts
+++ lib/picker/dev.ts
@@ -1,17 +1,1 @@
-import '@zui/icons';
-import '@zui/menu';
-import 'zui-dev';
-// import {Picker} from './src/main';
-
-// onPageUpdate(() => {
-//     new Picker('#picker3');
-//     new Picker('#picker4', {
-//         placeholder: '填写11',
-//         items: [
-//             {text: '复制', value: '1'},
-//             {text: '复制2', value: '2'},
-//         ],
-//         defaultValue: '1',
-//         onSelect: () => {console.log(111);},
-//     });
-// });
+import './src/main';
