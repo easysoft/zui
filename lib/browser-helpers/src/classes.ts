0f13e14e0a8b545b42a34e66c62425a2ae955108
Hao Sun <sunhao@easycorp.ltd>
2022-12-16 19:43:47 +0800

    * browser-helpers: add helper method to get class list and refactor classes method.

--- lib/browser-helpers/src/classes.ts
+++ lib/browser-helpers/src/classes.ts
@@ -1,23 +1,52 @@
 export type ClassNameLike = null | undefined | string | {[key: string]: unknown} | (() => ClassNameLike) | ClassNameLike[];
 
-export const classes = (...args: ClassNameLike[]): (string | undefined) => {
-    const classNames = args.map(arg => {
-        if (Array.isArray(arg)) {
-            return classes(...arg);
+export type ClassNameSetting = [name: string, toggle: boolean];
+
+export function getClassList(...args: ClassNameLike[]): ClassNameSetting[] {
+    const settings: ClassNameSetting[] = [];
+
+    /* Indexes map to keep classname's order */
+    const indexes = new Map<string, number>();
+
+    const setSetting = (name: string | [name: string, toggle?: unknown], toggle?: unknown) => {
+        if (Array.isArray(name)) {
+            toggle = name[1];
+            name = name[0];
         }
+        if (!name.length) {
+            return;
+        }
+        const index = indexes.get(name);
+        if (typeof index === 'number') {
+            settings[index][1] = !!toggle;
+        } else {
+            indexes.set(name, settings.length);
+            settings.push([name, !!toggle]);
+        }
+    };
+
+    args.forEach((arg) => {
         if (typeof arg === 'function') {
-            return classes(arg());
+            arg = arg();
         }
-        if (arg !== null && typeof arg === 'object') {
-            return Object.keys(arg).filter(className => {
-                const condition = arg[className];
-                if (typeof condition === 'function') {
-                    return !!(condition as () => boolean)();
-                }
-                return !!condition;
-            }).join(' ');
+
+        if (Array.isArray(arg)) {
+            getClassList(...arg).forEach(setSetting);
+        } else if (arg && typeof arg === 'object') {
+            Object.entries(arg).forEach(setSetting);
+        } else if (typeof arg === 'string') {
+            arg.split(' ').forEach(name => setSetting(name, true));
         }
-        return arg;
-    }).filter(x => (typeof x === 'string') && x.length);
-    return classNames.length ? classNames.join(' ') : undefined;
+    });
+
+    return settings.sort((a, b) => (indexes.get(a[0]) || 0) - (indexes.get(b[0]) || 0));
+}
+
+export const classes = (...args: ClassNameLike[]): string => {
+    return getClassList(...args).reduce<string[]>((classList, [name, toggle]) => {
+        if (toggle) {
+            classList.push(name);
+        }
+        return classList;
+    }, []).join(' ');
 };
