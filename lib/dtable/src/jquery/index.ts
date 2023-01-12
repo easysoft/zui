7827653b02ef0a44dbe5a2f741d79784e9d04747
Hao Sun <sunhao@easycorp.ltd>
2023-01-12 16:34:00 +0800

    * dtable: fix jquery helper name.

--- lib/dtable/src/jquery/index.ts
+++ lib/dtable/src/jquery/index.ts
@@ -1,6 +1,6 @@
 import {createJQueryFn} from '@zui/com-helpers/src/helpers/create-jquery-fn';
 import {DTable} from '../vanilla';
 
-createJQueryFn(DTable);
+createJQueryFn(DTable, 'DTable');
 
 export default {};
