94a29275fa564d9003d3e1aebfb383bc456312ea
Hao Sun <sunhao@easycorp.ltd>
2023-03-25 17:06:44 +0800

    * action-menu: remove type attr

--- lib/action-menu/src/component/action-custom.tsx
+++ lib/action-menu/src/component/action-custom.tsx
@@ -1,6 +1,6 @@
 import {CustomRender} from '@zui/com-helpers/src/helpers/custom-render';
 import {ActionCustomProps} from '../types/action-custom-props';
 
-export function ActionCustom(props: ActionCustomProps) {
+export function ActionCustom({type, ...props}: ActionCustomProps) {
     return <CustomRender {...props} />;
 }
