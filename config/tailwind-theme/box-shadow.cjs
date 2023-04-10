9efa16e250caec17e373c6cd25797e9f2bfc6e8e
Hao Sun <sunhao@easycorp.ltd>
2023-04-10 15:41:18 +0800

    * tailwind: change box-shadow config

--- config/tailwind-theme/box-shadow.cjs
+++ config/tailwind-theme/box-shadow.cjs
@@ -3,7 +3,7 @@ module.exports = {
     DEFAULT: '0 1px 4px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
     md: '0 2px 8px -1px rgb(0 0 0 / 0.1), 0 1px 4px -2px rgb(0 0 0 / 0.1)',
     lg: '0 4px 16px -4px rgb(0 0 0 / 0.1), 0 2px 6px -4px rgb(0 0 0 / 0.1)',
-    xl: '0 6px 32px -6px rgb(0 0 0 / 0.2), 0 4px 10px -6px rgb(0 0 0 / 0.2)',
+    xl: '0 6px 32px -6px rgb(0 0 0 / 0.15), 0 4px 10px -6px rgb(0 0 0 / 0.15)',
     '2xl': '0 25px 50px -12px rgb(0 0 0 / 0.25)',
     inner: 'inset 0 2px 4px 0 rgb(0 0 0 / 0.05)',
     none: 'none',
