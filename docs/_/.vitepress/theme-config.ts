0cdccb1b651628922895087a46a2f0ff7bc72ad3
sunhao <sunhao@easycorp.ltd>
2023-04-29 14:03:48 +0800

    * docs: add search bar

--- docs/_/.vitepress/theme-config.ts
+++ docs/_/.vitepress/theme-config.ts
@@ -7,6 +7,27 @@ import zuiLib from '../public/zui-libs';
 
 const themeConfig: DefaultTheme.Config = {
     logo: '/favicon.svg',
+    outline: [2, 3],
+    search: {
+        provider: 'local',
+        options: {
+            translations: {
+                button: {
+                    buttonText: '搜索文档',
+                    buttonAriaLabel: '搜索文档'
+                },
+                modal: {
+                    noResultsText: '无法找到相关结果',
+                    resetButtonTitle: '清除查询条件',
+                    footer: {
+                        selectText: '选择',
+                        navigateText: '切换',
+                        closeText: '关闭'
+                    }
+                }
+            }
+        }
+    },
     nav: createNav(),
     socialLinks: [
         {icon: 'github', link: 'https://github.com/easysoft/zui'}
