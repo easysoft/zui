2a1cb0364b1c4fe5632eb5c29a930300cb53e5ae
sunhao <sunhao@easycorp.ltd>
2023-04-29 14:03:14 +0800

    * docs: optimize UI

--- docs/_/.vitepress/theme/index.ts
+++ docs/_/.vitepress/theme/index.ts
@@ -1,12 +1,17 @@
 import DefaultTheme from 'vitepress/theme';
 // import DynamicLayout from './components/DynamicLayout.vue';
+import Example from './components/Example.vue';
 import './tailwind.css';
 import './vars.css';
 import './whyframe.css';
 import './style.css';
 
 export default {
-    ...DefaultTheme,
+    extends: DefaultTheme,
+
+    enhanceApp({app}) {
+        app.component('Example', Example);
+    },
 
     // use our custom layout component that we'll create next
     // Layout: DynamicLayout
