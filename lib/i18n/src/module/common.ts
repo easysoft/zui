5f26300995d6d7d04b4cb97e1c5c68634f3ca76c
sunhao <sunhao@easycorp.ltd>
2023-05-12 13:27:03 +0800

    * i18n: add more common language data.

--- lib/i18n/src/module/common.ts
+++ lib/i18n/src/module/common.ts
@@ -2,15 +2,21 @@ import {addI18nMap} from './i18n';
 
 addI18nMap({
     'zh_cn': {
-        'confirm': '确定',
-        'cancel': '取消',
+        confirm: '确定',
+        cancel: '取消',
+        delete: '删除',
+        add: '添加',
     },
     'zh_tw': {
-        'confirm': '確定',
-        'cancel': '取消',
+        confirm: '確定',
+        cancel: '取消',
+        delete: '刪除',
+        add: '添加',
     },
     en: {
-        'confirm': 'Confirm',
-        'cancel': 'Cancel',
+        confirm: 'Confirm',
+        cancel: 'Cancel',
+        delete: 'Delete',
+        add: 'Add',
     },
 });
