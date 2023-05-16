a2fae22c231e05abc9213de1bf1932a0de25cbfe
sunhao <sunhao@easycorp.ltd>
2023-05-16 10:32:50 +0800

    * dtable: refactor rich plugin.

--- lib/dtable/src/plugins/rich/index.tsx
+++ lib/dtable/src/plugins/rich/index.tsx
@@ -2,36 +2,36 @@ import {JSX, ComponentChildren, h as _h} from 'preact';
 import {formatString} from '@zui/helpers/src/format-string';
 import {formatDate} from '@zui/helpers/src/date-helper';
 import {definePlugin} from '../../helpers/shared-plugins';
+import {Avatar} from '@zui/avatar/src/component';
 import './style.css';
+import type {AvatarOptions} from '@zui/avatar/src/types';
 import type {DateLike} from '@zui/helpers/src/date-helper';
 import type {DTablePlugin, RowInfo, ColInfo, DTableWithPlugin, CustomRenderResultList} from '../../types';
-
-export type DTableActionButton = {
-    action: string;
-} & Partial<{
-    icon: string,
-    disabled: boolean,
-    title: string,
-    className: string,
-}>;
+import {classes} from '@zui/browser-helpers/src/classes';
 
 export type ColLinkSetting = string | ({url: string} & JSX.HTMLAttributes<HTMLAnchorElement>) | ((info: {row: RowInfo, col: ColInfo}) => string | ({url: string} & JSX.HTMLAttributes<HTMLAnchorElement>));
 
+export type ColDateFormatSetting = string | ((value: unknown, info: {row: RowInfo, col: ColInfo}) => string);
+
+export type ColFormatSetting = string | ((value: unknown, info: {row: RowInfo, col: ColInfo}) => string);
+
+export type ColHTMLSetting = boolean | string | ((value: unknown, info: {row: RowInfo, col: ColInfo}) => string);
+
 export type DTableRichTypes = {
     col: Partial<{
-        link: ColLinkSetting;
-        avatarWithName: string;
-        avatarClass: string;
-        avatarKey: string;
+        avatarClass?: string,
+        avatarKey?: string,
+        avatarCodeKey?: string,
+        avatarProps?: AvatarOptions,
+        avatarNameKey?: string;
         circleSize: number;
         circleBgColor: string;
         circleColor: string;
         circleBorderSize: number;
-        actionBtnTemplate: string;
-        actionBtnData: Record<string, Omit<DTableActionButton, 'action'>>,
-        actionBtnClass: string;
-        format: string | ((value: unknown, info: {row: RowInfo, col: ColInfo}) => string);
-        html: string | ((value: unknown, info: {row: RowInfo, col: ColInfo}) => string);
+        link: ColLinkSetting;
+        format: ColFormatSetting;
+        formatDate: ColDateFormatSetting;
+        html: ColHTMLSetting;
     }>,
 };
 
@@ -51,7 +51,7 @@ export function renderLink(link: ColLinkSetting | undefined, info: {row: RowInfo
     return <a href={formatString(url, info.row.data)} {...linkProps}>{content}</a>;
 }
 
-export function renderFormat(format: string | ((value: unknown, info: {row: RowInfo, col: ColInfo}) => string) | undefined, info: {row: RowInfo, col: ColInfo}, value?: unknown) {
+export function renderFormat(format: ColFormatSetting | undefined, info: {row: RowInfo, col: ColInfo}, value?: unknown) {
     if (format === undefined || format === null) {
         return;
     }
@@ -62,7 +62,7 @@ export function renderFormat(format: string | ((value: unknown, info: {row: RowI
     return formatString(format, value);
 }
 
-export function renderDatetime(format: string | ((value: unknown, info: {row: RowInfo, col: ColInfo}) => string), info: {row: RowInfo, col: ColInfo}, value?: unknown) {
+export function renderDatetime(format: ColDateFormatSetting, info: {row: RowInfo, col: ColInfo}, value?: unknown) {
     value = value ?? info.row.data?.[info.col.name];
     if (typeof format === 'function') {
         format = format(value, info);
@@ -82,14 +82,27 @@ export function renderLinkCell(result: CustomRenderResultList, info: {row: RowIn
 export function renderFormatCell(result: CustomRenderResultList, info: {row: RowInfo, col: ColInfo}) {
     const {format} = info.col.setting;
     if (format) {
-        result[0] = renderFormat(format as string | ((value: unknown, info: {row: RowInfo, col: ColInfo}) => string) | undefined, info, result[0]);
+        result[0] = renderFormat(format as ColFormatSetting, info, result[0]);
     }
     return result;
 }
 
-export function renderDatetimeCell(result: CustomRenderResultList, info: {row: RowInfo, col: ColInfo}, defaultFormat = '[yyyy-]MM-dd hh:mm') {
+export function renderDatetimeCell(result: CustomRenderResultList, info: {row: RowInfo, col: ColInfo}, defaultFormat: ColDateFormatSetting = '[yyyy-]MM-dd hh:mm') {
     const {format = defaultFormat} = info.col.setting;
-    result[0] = renderDatetime(format as string | ((value: unknown, info: {row: RowInfo, col: ColInfo}) => string), info, result[0]);
+    result[0] = renderDatetime(format as ColDateFormatSetting, info, result[0]);
+    return result;
+}
+
+export function renderHtmlCell(result: CustomRenderResultList, info: {row: RowInfo, col: ColInfo}, defaultHtml: ColHTMLSetting = false) {
+    const {html = defaultHtml} = info.col.setting;
+    if (html === false) {
+        return result;
+    }
+    const content = result[0];
+    const htmlCode = html === true ? content : renderFormat(html as ColFormatSetting, info, content);
+    result[0] = {
+        html: htmlCode,
+    };
     return result;
 }
 
@@ -98,32 +111,28 @@ const richPlugin: DTablePlugin<DTableRichTypes> = {
     colTypes: {
         html: {
             onRenderCell(result, info) {
-                const {html} = info.col.setting;
-                const content = result[0];
-                const htmlCode = html === undefined ? content : renderFormat(html, info, content);
-                result[0] = {
-                    html: htmlCode,
-                };
-                return result;
+                return renderHtmlCell(result, info, true);
             },
-        },
-        link: {
-            onRenderCell: renderLinkCell,
         },
         avatar: {
             onRenderCell(result, {col, row}) {
                 const {data: rowData} = row;
-                const {avatarWithName, avatarClass = 'size-xs rounded-full', avatarKey = `${col.name}Avatar`} = col.setting;
-                const avatar = (
-                    <div className={`avatar ${avatarClass} flex-none`}>
-                        <img src={rowData ? (rowData[avatarKey] as string) : ''} />
-                    </div>
-                );
-                if (avatarWithName) {
-                    result.unshift(avatar);
-                } else {
-                    result[0] = avatar;
+                const text = rowData ? (rowData[col.name] as string) : undefined;
+                if (!text?.length) {
+                    return result;
                 }
+                const {avatarClass = 'rounded-full', avatarKey = `${col.name}Avatar`, avatarProps, avatarCodeKey, avatarNameKey = `${col.name}Name`} = col.setting;
+                const name = (rowData ? (rowData[avatarNameKey] as string) : text) || result[0];
+                const props = {
+                    size: 'xs',
+                    className: classes(avatarClass, avatarProps?.className, 'flex-none'),
+                    src: rowData ? (rowData[avatarKey] as string) : undefined,
+                    text: name,
+                    code: avatarCodeKey ? (rowData ? (rowData[avatarCodeKey] as string) : undefined) : text,
+                    ...avatarProps,
+                } as AvatarOptions;
+
+                result[0] = <Avatar {...props} />;
                 return result;
             },
         },
@@ -159,9 +168,20 @@ const richPlugin: DTablePlugin<DTableRichTypes> = {
                 return renderDatetimeCell(result, info, 'hh:mm');
             },
         },
-        format: {
-            onRenderCell: renderFormatCell,
-        },
+    },
+    onRenderCell(result, info) {
+        const {formatDate: dateFormat, html} = info.col.setting;
+        if (dateFormat) {
+            result = renderDatetimeCell(result, info, dateFormat);
+        }
+        result = renderFormatCell(result, info);
+        if (html) {
+            result = renderHtmlCell(result, info);
+        } else {
+            result = renderLinkCell(result, info);
+        }
+
+        return result;
     },
 };
 
