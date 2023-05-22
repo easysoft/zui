e788f9a9481d16eaeefbcfb612ce5a9075901be3
sunhao <sunhao@easycorp.ltd>
2023-05-22 14:16:38 +0800

    * dashboard: change dev example.

--- lib/dashboard/dev.ts
+++ lib/dashboard/dev.ts
@@ -2,22 +2,24 @@ import 'preact/debug';
 import 'zui-dev';
 import '@zui/toolbar';
 import '@zui/panel';
+import '@zui/table';
+import '@zui/dtable';
 import {Dashboard} from './src/main';
 
 onPageLoad(() => {
     const dashboard = new Dashboard('#dashboard', {
         blocks: [
-            {id: 1, title: '1'},
-            {id: 2, title: '2', size: 'md'},
-            {id: 3, title: '3', size: {width: 4, height: 4}},
-            {id: 4, title: '4', placeholder: 'placeholder'},
-            {id: 5, title: '5'},
-            {id: 6, title: '6', left: 8, top: 0},
-            {id: 7, title: '7', size: 'smWide'},
-            {id: 8, title: '8', size: 'xs'},
-            {id: 9, title: '9', size: 'xsLong'},
+            {id: 1},
+            {id: 2, size: 'md'},
+            {id: 3, size: {width: 4, height: 4}},
+            {id: 4, placeholder: 'placeholder'},
+            {id: 5},
+            {id: 6, left: 8, top: 0},
+            {id: 7, size: 'smWide'},
+            {id: 8, size: 'xs'},
+            {id: 9, size: 'xsLong'},
         ],
-        // blockFetch: '/lib/dashboard/public/block.html?block={id}',
+        blockFetch: '/lib/dashboard/public/block-{id}.html',
     });
     console.log('> dashboard', dashboard);
 });
