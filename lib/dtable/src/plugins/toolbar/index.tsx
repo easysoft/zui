f8b9398b35d8f7c09cb0b5591bb698ee01da0eb0
sunhao <sunhao@easycorp.ltd>
2023-05-24 15:39:36 +0800

    * dtable: set default gap to toolbar.

--- lib/dtable/src/plugins/toolbar/index.tsx
+++ lib/dtable/src/plugins/toolbar/index.tsx
@@ -1,23 +1,28 @@
 import {Toolbar} from '@zui/toolbar/src/component/toolbar';
-import type {ToolbarOptions} from '@zui/toolbar/src/types';
 import {definePlugin} from '../../helpers/shared-plugins';
+import type {ToolbarOptions} from '@zui/toolbar/src/types';
 import type {DTablePlugin} from '../../types/plugin';
+import type {DTableCheckable, DTableCheckableTypes} from '../checkable';
 
 export type DTableToolbarTypes = {
     options: Partial<{
-        footToolbar: ToolbarOptions
+        footToolbar: ToolbarOptions,
+        showToolbarOnChecked: boolean,
     }>
 };
 
 /**
  * @todo auto calculate column width by toolbar setting
  */
-const toolbarPlugin: DTablePlugin<DTableToolbarTypes> = {
+const toolbarPlugin: DTablePlugin<DTableToolbarTypes, [DTableCheckableTypes]> = {
     name: 'toolbar',
     footer: {
         toolbar() {
-            const {footToolbar} = this.options;
-            return [footToolbar ? <Toolbar {...footToolbar} /> : null];
+            const {footToolbar, showToolbarOnChecked} = this.options;
+            if (showToolbarOnChecked && !(this as DTableCheckable).getChecks().length) {
+                return [];
+            }
+            return [footToolbar ? <Toolbar gap={2} {...footToolbar} /> : null];
         },
     },
 };
