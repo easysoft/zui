e219eaf82323b639334bd1657d4adab396895965
sunhao <sunhao@easycorp.ltd>
2023-05-31 16:58:49 +0800

    * avatar: refactor with @zui/core.

--- lib/avatar/src/vanilla/index.ts
+++ lib/avatar/src/vanilla/index.ts
@@ -1,9 +1,9 @@
-import {ComponentFromReact} from '@zui/com-helpers/src/helpers/component-react';
+import {ComponentFromReact} from '@zui/core';
 import {Avatar as AvatarReact} from '../component';
 import {AvatarOptions} from '../types';
 
 export class Avatar extends ComponentFromReact<AvatarOptions, AvatarReact> {
-    static NAME = 'avatar';
+    static NAME = 'Avatar';
 
     static Component = AvatarReact;
 }
