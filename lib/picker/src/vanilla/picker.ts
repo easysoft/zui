8bdfffcfe4d3b87b7dcf762868a2024d99fad026
sunhao <sunhao@easycorp.ltd>
2023-05-31 17:00:12 +0800

    * picker: refactor with @zui/core.

--- lib/picker/src/vanilla/picker.ts
+++ lib/picker/src/vanilla/picker.ts
@@ -1,9 +1,9 @@
-import {ComponentFromReact} from '@zui/com-helpers/src/helpers/component-react';
+import {ComponentFromReact} from '@zui/core';
 import {Picker as PickerReact} from '../component';
 import {PickerOptions} from '../types';
 
 export class Picker extends ComponentFromReact<PickerOptions, PickerReact> {
-    static NAME = 'picker';
+    static NAME = 'Picker';
 
     static Component = PickerReact;
 }
