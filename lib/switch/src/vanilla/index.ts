085b2a899bbfb5d8ef2b4b91559bc825e45f6507
sunhao <sunhao@easycorp.ltd>
2023-05-31 17:00:25 +0800

    * switch: refactor with @zui/core.

--- lib/switch/src/vanilla/index.ts
+++ lib/switch/src/vanilla/index.ts
@@ -1,9 +1,9 @@
-import {ComponentFromReact} from '@zui/com-helpers/src/helpers/component-react';
+import {ComponentFromReact} from '@zui/core';
 import {Switch as SwitchReact} from '../component/switch';
 import {SwitchProps} from '../types';
 
 export class Switch extends ComponentFromReact<SwitchProps, SwitchReact> {
-    static NAME = 'switch';
+    static NAME = 'Switch';
 
     static Component = SwitchReact;
 }
