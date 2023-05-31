ebbf900a5af671068483a0795feb42e1b0f30d35
sunhao <sunhao@easycorp.ltd>
2023-05-31 17:00:04 +0800

    * pager: refactor with @zui/core.

--- lib/pager/src/jquery/index.ts
+++
@@ -1,6 +0,0 @@
-import {createJQueryFn} from '@zui/com-helpers/src/helpers/create-jquery-fn';
-import {Pager} from '../vanilla';
-
-createJQueryFn(Pager);
-
-export default {};
--- lib/pager/src/vanilla/index.ts
+++ lib/pager/src/vanilla/index.ts
@@ -1,10 +1,10 @@
-import {ComponentFromReact} from '@zui/com-helpers/src/helpers/component-react';
+import {ComponentFromReact} from '@zui/core';
 import type {ActionBasicProps} from '@zui/action-menu/src/types/action-basic-props';
 import {Pager as PagerReact} from '../component/pager';
 import {PagerOptions, PagerItemOptions} from '../types';
 
 export class Pager<T extends ActionBasicProps = PagerItemOptions> extends ComponentFromReact<PagerOptions<T>, PagerReact<T>> {
-    static NAME = 'pager';
+    static NAME = 'Pager';
 
     static Component = PagerReact;
 }
