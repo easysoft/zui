ed97f3b6659203fc62ea6017cd58cf45458ce3f1
sunhao <sunhao@easycorp.ltd>
2023-05-31 17:00:19 +0800

    * progress-circle: refactor with @zui/core.

--- lib/progress-circle/src/vanilla/index.ts
+++ lib/progress-circle/src/vanilla/index.ts
@@ -1,9 +1,9 @@
-import {ComponentFromReact} from '@zui/com-helpers/src/helpers/component-react';
+import {ComponentFromReact} from '@zui/core';
 import {ProgressCircle as ProgressCircleReact} from '../component/progress-circle';
 import {ProgressCircleProps} from '../component/progress-circle';
 
-export class ProgressCircle extends ComponentFromReact<ProgressCircleProps, ProgressCircleReact<ProgressCircleProps>> {
-    static NAME = 'table-sorter';
+export class ProgressCircle extends ComponentFromReact<ProgressCircleProps, ProgressCircleReact> {
+    static NAME = 'ProgressCircle';
 
     static Component = ProgressCircleReact;
 }
