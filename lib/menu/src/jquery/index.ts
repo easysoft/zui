62b8e2e4cae6cfc06211a3de67c67fe9c53d7fd1
sunhao <sunhao@easycorp.ltd>
2023-05-31 16:59:44 +0800

    * menu: refactor with @zui/core.

--- lib/menu/src/jquery/index.ts
+++
@@ -1,6 +0,0 @@
-import {createJQueryFn} from '@zui/com-helpers/src/helpers/create-jquery-fn';
-import {Menu} from '../vanilla';
-
-createJQueryFn(Menu);
-
-export default {};
--- lib/menu/src/vanilla/index.ts
+++ lib/menu/src/vanilla/index.ts
@@ -1,9 +1,9 @@
-import {ComponentFromReact} from '@zui/com-helpers/src/helpers/component-react';
+import {ComponentFromReact} from '@zui/core';
 import {Menu as MenuReact} from '../component/menu';
 import {MenuOptions, MenuItemOptions} from '../types';
 
 export class Menu extends ComponentFromReact<MenuOptions, MenuReact<MenuItemOptions>> {
-    static NAME = 'menu';
+    static NAME = 'Menu';
 
     static Component = MenuReact;
 }
