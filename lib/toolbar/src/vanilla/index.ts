34be5e16166478d98a072e186c325d40946f35cb
sunhao <sunhao@easycorp.ltd>
2023-05-31 17:00:44 +0800

    * toolbar: refactor with @zui/core.

--- lib/toolbar/src/vanilla/index.ts
+++ lib/toolbar/src/vanilla/index.ts
@@ -1,10 +1,10 @@
-import {ComponentFromReact} from '@zui/com-helpers/src/helpers/component-react';
+import {ComponentFromReact} from '@zui/core';
 import type {ActionBasicProps} from '@zui/action-menu/src/types/action-basic-props';
 import {Toolbar as ToolbarReact} from '../component/toolbar';
 import {ToolbarOptions, ToolbarItemOptions} from '../types';
 
 export class Toolbar<T extends ActionBasicProps = ToolbarItemOptions> extends ComponentFromReact<ToolbarOptions<T>, ToolbarReact<T>> {
-    static NAME = 'toolbar';
+    static NAME = 'Toolbar';
 
     static Component = ToolbarReact;
 }
