2e9de1b5e28e8cdbd705fe410e40ab5f955e2f9e
sunhao <sunhao@easycorp.ltd>
2023-05-31 17:00:00 +0800

    * nav: refactor with @zui/core.

--- lib/nav/src/vanilla/index.ts
+++ lib/nav/src/vanilla/index.ts
@@ -1,11 +1,12 @@
-import {ComponentFromReact} from '@zui/com-helpers/src/helpers/component-react';
+import {ComponentFromReact} from '@zui/core';
 import type {ActionBasicProps} from '@zui/action-menu/src/types/action-basic-props';
 import {Nav as NavReact} from '../component/nav';
 import {NavItemOptions} from '../types/nav-item-options';
 import {NavOptions} from '../types/nav-options';
 
-export class Nav<T extends ActionBasicProps = NavItemOptions> extends ComponentFromReact<NavOptions<T>, NavReact<T>> {
-    static NAME = 'nav';
+
+export class Nav<O extends ActionBasicProps = NavItemOptions> extends ComponentFromReact<NavOptions<O>, NavReact<O>> {
+    static NAME = 'Nav';
 
     static Component = NavReact;
 }
