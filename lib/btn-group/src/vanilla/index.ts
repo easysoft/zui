8eec50b88bd05f8b75192408d41b3ae7cc84c67d
sunhao <sunhao@easycorp.ltd>
2023-05-31 16:58:55 +0800

    * btn-group: refactor with @zui/core.

--- lib/btn-group/src/vanilla/index.ts
+++ lib/btn-group/src/vanilla/index.ts
@@ -1,9 +1,9 @@
-import {ComponentFromReact} from '@zui/com-helpers/src/helpers/component-react';
+import {ComponentFromReact} from '@zui/core';
 import {BtnGroup as BtnGroupReact} from '../component/btn-group';
 import {BtnGroupOptions} from '../types/btn-group-options';
 
 export class BtnGroup extends ComponentFromReact<BtnGroupOptions, BtnGroupReact> {
-    static NAME = 'btngroup';
+    static NAME = 'BtnGroup';
 
     static Component = BtnGroupReact;
 }
