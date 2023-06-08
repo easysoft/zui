01e4aff7907205d3ae5e67a1b6856a07963ee1fc
sunhao <sunhao@easycorp.ltd>
2023-06-08 15:54:31 +0800

    - ajax-form: remove files.

--- lib/ajax-form/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ajax-form-options';
-export * from './ajax-form-result';
-export * from './ajax-form-events';
-export * from './ajax-submit-options';
--- lib/ajax-form/src/vanilla/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './ajax-form';
-export * from './ajax-submit';
