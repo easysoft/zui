8adf1df64ce66c4fab9a734cf70b22982e0d7269
sunhao <sunhao@easycorp.ltd>
2023-06-15 09:49:12 +0800

    * tree: refactor dev example.

--- lib/tree/dev.ts
+++ lib/tree/dev.ts
@@ -7,75 +7,68 @@ import '@zui/checkbox';
 import {Tree} from './src/main';
 
 onPageUpdate(() => {
+    const items = [
+        {
+            text: '研发',
+            url: '#研发',
+            items: [
+                {
+                    text: '大产品',
+                    url: '#大产品',
+                    items: [
+                        {text: '前端', url: '#前端'},
+                        {text: '后端', url: '#后端'},
+                    ],
+                },
+                {text: '桌面端', url: '#桌面端'},
+                {text: '移动端', url: '#移动端'},
+                {text: '测试', url: '#测试'},
+                {text: '运维', url: '#运维'},
+            ],
+        },
+        {text: '市场', url: '#市场'},
+        {text: '技术支持', url: '#技术支持'},
+        {text: '财务', url: '#财务'},
+        {text: '行政', url: '#行政'},
+    ];
     const tree = new Tree('#tree', {
-        checkbox: true,
+        // checkbox: true,
         indent: 20,
         collapsedIcon: 'folder-close',
         expandedIcon: 'folder-open',
         normalIcon: 'file-o',
         hover: true,
-        itemActions: [
-            {
-                key: 'edit',
-                icon: 'edit',
-                hint: '编辑',
-                onClick: (e) => console.log(e),
-            },
-            {
-                key: 'delete',
-                icon: 'trash',
-                hint: '删除',
-                onClick: (e) => console.log(e),
-            },
-            {
-                type: 'dropdown',
-                key: 'more',
-                icon: 'ellipsis-v',
-                caret: false,
-                hint: '删除',
-                dropdown: {
-                    placement: 'bottom-end',
-                    items: [
-                        {text: '分享'},
-                        {text: '下载'},
-                    ],
+        itemActions: {
+            items: [
+                {
+                    key: 'edit',
+                    icon: 'edit',
+                    hint: '编辑',
+                    onClick: (e) => console.log(e),
                 },
-            },
-        ],
-        items: [
-            {
-                text: '首页',
-                icon: 'home',
-                url: '#test',
-            },
-            {
-                text: '动态',
-                items: [
-                    {
-                        text: '新闻',
-                        checked: true,
-                    },
-                    {
-                        text: '行业',
-                    },
-                    {
-                        text: '发布',
-                        url: '#release',
+                {
+                    key: 'delete',
+                    icon: 'trash',
+                    hint: '删除',
+                    onClick: (e) => console.log(e),
+                },
+                {
+                    type: 'dropdown',
+                    key: 'more',
+                    icon: 'ellipsis-v',
+                    caret: false,
+                    hint: '删除',
+                    dropdown: {
+                        placement: 'bottom-end',
                         items: [
-                            {
-                                text: '桌面端',
-                            },
-                            {
-                                text: '移动端',
-                            },
+                            {text: '分享'},
+                            {text: '下载'},
                         ],
                     },
-                ],
-            },
-            {
-                text: '博客',
-            },
-        ],
+                },
+            ],
+        },
+        items,
     });
     console.log('> tree', tree);
 });
