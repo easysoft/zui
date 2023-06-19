ab1c025b9787592b97dfaec98ab155da83c187eb
sunhao <sunhao@easycorp.ltd>
2023-06-19 11:10:36 +0800

    * config: optimize shadows config.

--- config/tailwind-theme/box-shadow.cjs
+++ config/tailwind-theme/box-shadow.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-    sm: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
-    DEFAULT: '0 1px 4px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
-    md: '0 2px 8px -1px rgb(0 0 0 / 0.1), 0 1px 4px -2px rgb(0 0 0 / 0.1)',
-    lg: '0 4px 16px -4px rgb(0 0 0 / 0.1), 0 2px 6px -4px rgb(0 0 0 / 0.1)',
-    xl: '0 6px 32px -6px rgb(0 0 0 / 0.15), 0 4px 10px -6px rgb(0 0 0 / 0.15)',
-    '2xl': '0 25px 50px -12px rgb(0 0 0 / 0.25)',
+    sm: '0 1px 2px 0 rgb(0 0 0 / 0.1), 0 0 2px -1px rgb(0 0 0 / 0.05)',
+    DEFAULT: '0 1px 4px 0 rgb(0 0 0 / 0.15), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+    md: '0 2px 6px -1px rgb(0 0 0 / 0.2), 0 1px 4px -2px rgb(0 0 0 / 0.15)',
+    lg: '0 5px 15px -3px rgb(0 0 0 / 0.2), 0 2px 6px -4px rgb(0 0 0 / 0.15)',
+    xl: '0 10px 25px -5px rgb(0 0 0 / 0.2), 0 4px 10px -6px rgb(0 0 0 / 0.15)',
+    '2xl': '0 10px 50px -10px rgb(0 0 0 / 0.4), 0 8px 20px -10px rgb(0 0 0 / 0.3)',
     inner: 'inset 0 2px 4px 0 rgb(0 0 0 / 0.05)',
     none: 'none',
 };
