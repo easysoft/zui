4b9f0a84a7ede76dc560bd61e658e5d0a6f22fef
sunhao <sunhao@easycorp.ltd>
2023-06-25 18:25:42 +0800

    * core: refactor delay helper param.

--- lib/core/src/helpers/delay.ts
+++ lib/core/src/helpers/delay.ts
@@ -1,4 +1,4 @@
-export const delay = (ms: number, setTimerID: (timerID: number) => void) => new Promise(resolve => {
+export const delay = (ms: number, setTimerID?: (timerID: number) => void) => new Promise(resolve => {
     const id = window.setTimeout(resolve, ms);
     if (setTimerID) {
         setTimerID(id);
