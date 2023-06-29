7a55c70fb74a20a910bd4d6e385ceb0941f38648
sunhao <sunhao@easycorp.ltd>
2023-06-29 21:42:17 +0800

    + core: add scrollIntoView helper.

--- lib/core/src/dom/index.ts
+++ lib/core/src/dom/index.ts
@@ -1,2 +1,3 @@
 export * from './is-visible';
 export * from './run-js';
+export * from './scroll-into-view';
---
+++ lib/core/src/dom/scroll-into-view.ts
@@ -0,0 +1,43 @@
+import {$, Cash, Selector} from '../cash';
+import {isVisible} from './is-visible';
+
+/**
+ * Options for {@link scrollIntoView}.
+ */
+type CashScrollIntoViewOptions = ScrollIntoViewOptions & {
+    ifNeeded?: boolean;
+};
+
+/**
+ * Scroll into view.
+ *
+ * @param selector Element selector to scroll into view.
+ * @param options  Options.
+ * @returns True if the element is visible.
+ * @see https://stackoverflow.com/a/26039199
+ */
+export function scrollIntoView(selector: Selector, options?: CashScrollIntoViewOptions): Cash {
+    const $element = $(selector);
+    const {ifNeeded = true, ...other} = options || {};
+    $element.each((_, ele) => {
+        if (ifNeeded && isVisible(ele, {fullyCheck: true})) {
+            return;
+        }
+        ele.scrollIntoView(other);
+    });
+    return $element;
+}
+
+/* Declare types. */
+declare module 'cash-dom' {
+    interface Cash {
+        scrollIntoView(options?: CashScrollIntoViewOptions): this;
+    }
+}
+
+/* Extend as $.fn.scrollIntoView() */
+$.fn.scrollIntoView = function (this: Cash, options?: CashScrollIntoViewOptions) {
+    return this.each((_, ele) => {
+        scrollIntoView(ele, options);
+    });
+};
