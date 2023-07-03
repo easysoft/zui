a9f1af069955b5416abb860d278dd68de7d177ac
sunhao <sunhao@easycorp.ltd>
2023-07-03 18:11:00 +0800

    * color-picker: refactor menu-popup to popup.

--- lib/color-picker/src/components/color-picker.tsx
+++ lib/color-picker/src/components/color-picker.tsx
@@ -9,7 +9,7 @@ export class ColorPicker extends Pick<PickState, ColorPickerOptions> {
     static defaultProps = {
         ...Pick.defaultProps,
         className: 'color-picker rounded btn square size-sm ghost',
-        popClass: 'color-picker-pop menu-popup',
+        popClass: 'color-picker-pop popup',
         colors: ['#ef4444', '#f97316', '#eab308', '#84cc16', '#22c55e', '#14b8a6', '#0ea5e9', '#6366f1', '#a855f7', '#d946ef', '#ec4899'],
         closeBtn: true,
         popWidth: 'auto',
