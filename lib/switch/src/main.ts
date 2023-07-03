dfb1fd73341735c7be054596870b9d0fffcbd0de
sunhao <sunhao@easycorp.ltd>
2023-07-03 20:35:44 +0800

    - switch: remove files.

--- lib/switch/src/main.ts
+++
@@ -1,2 +0,0 @@
-export * from './main-css';
-export * from './vanilla';
--- lib/switch/src/vanilla/index.ts
+++
@@ -1,9 +0,0 @@
-import {ComponentFromReact} from '@zui/core';
-import {Switch as SwitchReact} from '../component/switch';
-import {SwitchProps} from '../types';
-
-export class Switch extends ComponentFromReact<SwitchProps, SwitchReact> {
-    static NAME = 'Switch';
-
-    static Component = SwitchReact;
-}
