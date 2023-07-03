c5d1a3701b928990fad65b81901e94487501ba9b
sunhao <sunhao@easycorp.ltd>
2023-07-03 18:12:07 +0800

    * contextmenu: fix nested menu position.

--- lib/contextmenu/src/component/contextmenu.tsx
+++ lib/contextmenu/src/component/contextmenu.tsx
@@ -1,11 +1,15 @@
+import {classes, $} from '@zui/core';
 import type {MenuItemOptions} from '@zui/menu/src/types';
 import {Menu} from '@zui/menu/src/component/menu';
+import {MenuOptions} from '@zui/menu/src/types';
+import {flip, computePosition, shift, offset} from '@floating-ui/dom';
+import {ActionMenuNestedItemOptions} from '@zui/action-menu/src/types';
 import '@zui/css-icons/src/icons/caret.css';
 import '../style/index.css';
-import {flip, computePosition, ComputePositionConfig} from '@floating-ui/dom';
-import {classes} from '@zui/core';
 
 export class ContextMenu<T extends MenuItemOptions = MenuItemOptions> extends Menu<T> {
+    #layoutTimer = 0;
+
     get nestedTrigger() {
         return this.props.nestedTrigger || 'hover';
     }
@@ -18,45 +22,53 @@ export class ContextMenu<T extends MenuItemOptions = MenuItemOptions> extends Me
         return 'menu-context';
     }
 
-    protected getPopperOptions() {
-        const config: Partial<ComputePositionConfig> = {
-            middleware: [flip()],
-            placement: 'right-start',
-        };
-        return config;
-    }
-
-    protected getPopperElement() {
-        return this.ref.current?.parentElement as HTMLElement;
-    }
-
-    protected createPopper() {
-        const config = this.getPopperOptions();
-        if (this.ref.current) {
-            computePosition(this.getPopperElement(), this.ref.current, config).then(({x, y}) => {
-                Object.assign((this.ref.current as HTMLElement).style, {
-                    left: `${x}px`,
-                    top: `${y}px`,
-                    position: 'absolute',
-                });
-            });
+    protected layout() {
+        const element = this.ref.current as HTMLElement;
+        const trigger = element?.parentElement;
+        if (!element || !trigger) {
+            return;
         }
+
+        computePosition(trigger, element, {
+            placement: 'right-start',
+            middleware: [flip(), shift(), offset(1)],
+        }).then(({x, y}) => {
+            $(element).css({
+                left: x,
+                top: y,
+            });
+        });
+    }
+
+    getNestedMenuProps(items: ActionMenuNestedItemOptions[]): MenuOptions {
+        return {
+            ...super.getNestedMenuProps(items),
+            popup: true,
+        };
     }
 
     afterRender(firstRender: boolean) {
         super.afterRender(firstRender);
         if (this.props.controlledMenu) {
-            this.createPopper();
+            this.layout();
+            this.#layoutTimer = window.setTimeout(this.layout.bind(this), 100);
         }
     }
 
     beforeRender() {
         const options = super.beforeRender();
-        options.className = classes(options.className, 'menu-popup');
+        options.className = classes(options.className, 'popup');
         return options;
     }
 
     renderToggleIcon() {
         return <span class="contextmenu-toggle-icon caret-right" />;
+    }
+
+    componentWillUnmount(): void {
+        super.componentWillUnmount();
+        if (this.#layoutTimer) {
+            clearTimeout(this.#layoutTimer);
+        }
     }
 }
