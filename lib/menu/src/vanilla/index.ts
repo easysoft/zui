256e2ea2d03dff8c0dd63ba46e376cfac65d16f8
sunhao <sunhao@easycorp.ltd>
2023-07-04 20:36:24 +0800

    * menu: refactor.

--- lib/menu/src/vanilla/index.ts
+++ lib/menu/src/vanilla/index.ts
@@ -1,5 +1,5 @@
 import {ComponentFromReact} from '@zui/core';
-import {Menu as MenuReact} from '../component/menu';
+import {Menu as MenuReact} from '../component';
 import {MenuOptions, MenuItemOptions} from '../types';
 
 export class Menu extends ComponentFromReact<MenuOptions, MenuReact<MenuItemOptions>> {
