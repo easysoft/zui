37798b3aac96e91be1c818ea1a22c2d2f391fe41
sunhao <sunhao@easycorp.ltd>
2023-07-06 10:49:23 +0800

    * core: refactor ts types for i18n.

--- lib/core/src/i18n/i18n.ts
+++ lib/core/src/i18n/i18n.ts
@@ -27,9 +27,9 @@ export function addI18nMap(codeOrMap: I18nLangCode | I18nLangMap, values?: I18nV
     $.extend(true, globalLangMap, codeOrMap);
 }
 
-export function i18n(maps: I18nLangMap | (I18nLangMap | undefined)[] | undefined, key: string, defaultValue?: string, langCode?: I18nLangCode, globalPrefix?: string): string | undefined;
-export function i18n(maps: I18nLangMap | (I18nLangMap | undefined)[] | undefined, key: string, args?: string | (string | number)[] | Record<string, string | number>, defaultValue?: string, langCode?: I18nLangCode, globalPrefix?: string): string | undefined;
-export function i18n(maps: I18nLangMap | (I18nLangMap | undefined)[] | undefined, key: string, args?: string | (string | number)[] | Record<string, string | number>, defaultValue?: string, langCode?: I18nLangCode, globalPrefix?: string): string | undefined {
+export function i18n<T = string>(maps: I18nLangMap | (I18nLangMap | undefined)[] | undefined, key: string, defaultValue?: T, langCode?: I18nLangCode, globalPrefix?: string): T | undefined;
+export function i18n<T = string>(maps: I18nLangMap | (I18nLangMap | undefined)[] | undefined, key: string, args?: string | (string | number)[] | Record<string, string | number>, defaultValue?: T, langCode?: I18nLangCode, globalPrefix?: string): T | undefined;
+export function i18n<T = string>(maps: I18nLangMap | (I18nLangMap | undefined)[] | undefined, key: string, args?: string | (string | number)[] | Record<string, string | number>, defaultValue?: T | I18nLangCode, langCode?: I18nLangCode, globalPrefix?: string): T | undefined {
     if (!Array.isArray(maps)) {
         maps = globalLangMap ? [globalLangMap, maps] : [maps];
     } else if (globalLangMap) {
@@ -37,12 +37,12 @@ export function i18n(maps: I18nLangMap | (I18nLangMap | undefined)[] | undefined
     }
     if (typeof args === 'string') {
         globalPrefix = langCode;
-        langCode = defaultValue;
+        langCode = defaultValue as I18nLangCode;
         defaultValue = args;
         args = undefined;
     }
     const lang = langCode || globalLangCode;
-    let value: string | undefined;
+    let value: T | undefined;
     for (const map of maps) {
         if (!map) {
             continue;
@@ -52,22 +52,22 @@ export function i18n(maps: I18nLangMap | (I18nLangMap | undefined)[] | undefined
             continue;
         }
         const mapKey = (globalPrefix && map === globalLangMap) ? `${globalPrefix}.${key}` : key;
-        value = deepGet(mapValues, mapKey);
+        value = deepGet<T>(mapValues, mapKey);
         if (value !== undefined) {
             break;
         }
     }
     if (value === undefined) {
-        return defaultValue;
+        return defaultValue as T;
     }
     if (args) {
-        return formatString(value, ...(Array.isArray(args) ? args : [args]));
+        return formatString(value as string, ...(Array.isArray(args) ? args : [args])) as T;
     }
     return value;
 }
 
-export function getLang(key: string, args?: string | (string | number)[] | Record<string, string | number>, defaultValue?: string, langCode?: I18nLangCode) {
-    return i18n(undefined, key, args, defaultValue, langCode);
+export function getLang<T = string>(key: string, args?: string | (string | number)[] | Record<string, string | number>, defaultValue?: T, langCode?: I18nLangCode): T | undefined {
+    return i18n<T>(undefined, key, args, defaultValue, langCode);
 }
 
 i18n.addLang = addI18nMap;
