e5e63e02945a238a90ba94963a346cba0a6eaac3
sunhao <sunhao@easycorp.ltd>
2023-07-11 16:09:22 +0800

    * core: fix icon is object type not work.

--- lib/core/src/react/components/icon.tsx
+++ lib/core/src/react/components/icon.tsx
@@ -24,7 +24,6 @@ export function Icon(props: IconProps) {
         const {className: iconClass, ...iconOthers} = icon;
         classList.push(iconClass as string);
         Object.assign(others, iconOthers);
-        return <Icon className={classes(classList)} {...others} />;
     }
     return <i className={classes(classList)} {...others} />;
 }
