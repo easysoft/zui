0498235e19047b5b967f4f27ac3c82a2ab567774
sunhao <sunhao@easycorp.ltd>
2023-07-12 11:47:26 +0800

    * dtable: add more helper class to cell element.

--- lib/dtable/src/components/cells.tsx
+++ lib/dtable/src/components/cells.tsx
@@ -36,18 +36,26 @@ export function Cells({
 }: CellsProps) {
     const rowsList = Array.isArray(rows) ? rows : [rows];
     const firstRowTop = rowsList[0]?.top ?? 0;
+    const rowsCount = rowsList.length;
     return (
         <div
             className={classes('dtable-cells', className)}
             style={{top, left, width, height}}
         >
             <div className="dtable-cells-container" style={{left: -scrollLeft, top: -scrollTop + firstRowTop}}>
-                {rowsList.reduce<ComponentChild[]>((list, row) => {
-                    cols.forEach(col => {
+                {rowsList.reduce<ComponentChild[]>((list, row, rowIndex) => {
+                    const colsCount = cols.length;
+                    cols.forEach((col, colIndex) => {
                         list.push(
                             <CellComponent
                                 key={`${row.index}:${col.name}`}
-                                className={`is-${row.index % 2 ? 'odd' : 'even'}-row`}
+                                className={classes(
+                                    `is-${row.index % 2 ? 'odd' : 'even'}-row`,
+                                    colIndex ? '' : 'is-first-in-row',
+                                    colIndex === colsCount - 1 ? 'is-last-in-row' : '',
+                                    rowIndex ? '' : 'is-first-row',
+                                    rowIndex === rowsCount - 1 ? 'is-last-row' : '',
+                                )}
                                 col={col}
                                 row={row}
                                 top={row.top - firstRowTop}
