a0a9e969c4c47ff1d30c623483ad25bad35d36ed
sunhao <sunhao@easycorp.ltd>
2023-08-21 15:58:57 +0800

    * menu: refactor types.

--- lib/menu/src/component/search-menu.tsx
+++ lib/menu/src/component/search-menu.tsx
@@ -5,13 +5,14 @@ import {Menu} from './menu';
 import type {ComponentChild, JSX, VNode} from 'preact';
 import type {SearchMenuState, MenuItemOptions, SearchMenuOptions, MenuItemProps} from '../types';
 import type {SearchBoxOptions} from '@zui/search-box/src/types';
+import type {ActionMenuItemOptions} from '@zui/action-menu/src/types';
 
 const isItemMatch = (item: MenuItemProps & {keys?: string}, searchKeys: string[]) => {
     const {keys = '', text} = item;
     return !searchKeys.length || searchKeys.every(searchKey => keys.toLowerCase().includes(searchKey) || (typeof text === 'string' && text.toLowerCase().includes(searchKey)));
 };
 
-export class SearchMenu extends Menu<MenuItemOptions, SearchMenuOptions, SearchMenuState> {
+export class SearchMenu<T extends ActionMenuItemOptions = MenuItemOptions, O extends SearchMenuOptions<T> = SearchMenuOptions<T>, S extends SearchMenuState = SearchMenuState> extends Menu<T, O, S> {
     _searchKeys: string[] = [];
 
     _handleSearchChange = (search: string) => {
@@ -19,20 +20,19 @@ export class SearchMenu extends Menu<MenuItemOptions, SearchMenuOptions, SearchM
         this.setState({search});
     };
 
-    renderItem(props: Omit<SearchMenuOptions, 'items'> & {items: MenuItemOptions[];}, item: MenuItemOptions, index: number): JSX.Element | VNode<{}> | null {
+    renderItem(props: Omit<O, 'items'> & {items: T[];}, item: T, index: number): JSX.Element | VNode<{}> | null {
         if (!isItemMatch(item, this._searchKeys)) {
             return null;
         }
         return super.renderItem(props, item, index);
     }
 
-    beforeRender(): Omit<SearchMenuOptions, 'items'> & {items: MenuItemOptions[];} {
-        const options = super.beforeRender();
-        const {search = true} = options;
+    beforeRender(): Omit<O, 'items'> & {items: T[];} {
+        const {search = true, searchPlacement = 'top', ...options} = super.beforeRender();
         if (!search) {
-            return options;
+            return options as Omit<O, 'items'> & {items: T[];};
         }
-        options.className = classes(options.className, 'search-menu', `search-menu-on-${options.searchPlacement || 'top'}`);
+        options.className = classes(options.className, 'search-menu', `search-menu-on-${searchPlacement || 'top'}`);
         options.children = options.children ? (Array.isArray(options.children) ? options.children : [options.children]) : [];
         const searchOptions: SearchBoxOptions = {
             onChange: this._handleSearchChange,
@@ -43,6 +43,6 @@ export class SearchMenu extends Menu<MenuItemOptions, SearchMenuOptions, SearchM
         (options.children as ComponentChild[]).push(
             <SearchBox key="search" {...searchOptions} />,
         );
-        return options;
+        return options as Omit<O, 'items'> & {items: T[];};
     }
 }
--- lib/menu/src/types/search-menu-options.ts
+++ lib/menu/src/types/search-menu-options.ts
@@ -1,7 +1,9 @@
+import type {ActionBasicProps} from '@zui/action-menu/src/types';
 import type {SearchBoxOptions} from '@zui/search-box';
 import type {MenuOptions} from './menu-options';
+import type {MenuItemOptions} from './menu-item-options';
 
-export interface SearchMenuOptions extends MenuOptions {
+export interface SearchMenuOptions<T extends ActionBasicProps = MenuItemOptions> extends MenuOptions<T> {
     search?: SearchBoxOptions | boolean;
     searchPlacement?: 'bottom' | 'top';
 }
