ada3bef4c529a504fb0a4e334a88cc07aba5b567
sunhao <sunhao@easycorp.ltd>
2023-09-04 16:18:23 +0800

    * btn-group: refactor.

--- lib/btn-group/src/component/btn-group.tsx
+++ lib/btn-group/src/component/btn-group.tsx
@@ -3,15 +3,14 @@ import {Button} from '@zui/button/src/component';
 import {List} from '@zui/list/src/component';
 import {DropdownButton} from '@zui/dropdown/src/component';
 
-import type {RenderableProps} from 'preact';
-import type {ClassNameLike} from '@zui/core';
 import type {ListProps, Item} from '@zui/list';
 import type {BtnGroupOptions} from '../types';
 
-function getBtnProps(item: Item, props: ListProps) {
+export function getBtnProps(item: Item, props: ListProps) {
     const {style, rootClass, class: className, class: className2, ...others} = item;
-    const {btnProps = {}, btnType} = props as BtnGroupOptions;
+    const {size, btnProps = {}, btnType} = props as BtnGroupOptions;
     return {
+        size,
         btnType,
         ...btnProps,
         ...others,
@@ -32,8 +31,5 @@ export class BtnGroup<T extends BtnGroupOptions = BtnGroupOptions> extends List<
 
     static ITEM_NAME = '';
 
-    protected _getClassName(props: RenderableProps<T>): ClassNameLike {
-        const {size} = props;
-        return [super._getClassName(props), size ? `size-${size}` : ''];
-    }
+    static getBtnProps = getBtnProps;
 }
