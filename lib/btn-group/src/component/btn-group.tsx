7722cb02ec98fd2b34bb53e8e1241d3d22c94e83
sunhao <sunhao@easycorp.ltd>
2023-09-04 22:07:00 +0800

    * btn-group: refactor.

--- lib/btn-group/src/component/btn-group.tsx
+++ lib/btn-group/src/component/btn-group.tsx
@@ -6,25 +6,11 @@ import {DropdownButton} from '@zui/dropdown/src/component';
 import type {ListProps, Item} from '@zui/list';
 import type {BtnGroupOptions} from '../types';
 
-export function getBtnProps(item: Item, props: ListProps) {
-    const {style, rootClass, class: className, class: className2, ...others} = item;
-    const {size, btnProps = {}, btnType} = props as BtnGroupOptions;
-    return {
-        size,
-        btnType,
-        ...btnProps,
-        ...others,
-        style: btnProps.style ? $.extend({}, btnProps.style, style) : style,
-        className: [className, className2, rootClass, btnProps.className],
-        type: '',
-    };
-}
-
 export class BtnGroup<T extends BtnGroupOptions = BtnGroupOptions> extends List<T> {
     static ItemComponents: typeof List.ItemComponents = {
         ...List.ItemComponents,
-        item: [Button, getBtnProps],
-        dropdown: [DropdownButton, getBtnProps],
+        item: [Button, this.getBtnProps],
+        dropdown: [DropdownButton, this.getBtnProps],
     };
 
     static NAME = 'btn-group';
@@ -36,5 +22,17 @@ export class BtnGroup<T extends BtnGroupOptions = BtnGroupOptions> extends List<
     static defaultItemProps: Partial<Item> = {
     };
 
-    static getBtnProps = getBtnProps;
+    static getBtnProps(item: Item, props: ListProps) {
+        const {style, rootClass, class: className, class: className2, ...others} = item;
+        const {size, btnProps = {}, btnType} = props as BtnGroupOptions;
+        return {
+            size,
+            btnType,
+            ...btnProps,
+            ...others,
+            style: btnProps.style ? $.extend({}, btnProps.style, style) : style,
+            className: [className, className2, rootClass, btnProps.className],
+            type: '',
+        };
+    }
 }
