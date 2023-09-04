4290a980130361bdc4714e35d631860f2dd87020
sunhao <sunhao@easycorp.ltd>
2023-09-04 15:12:55 +0800

    * btn-group: refactor.

--- lib/btn-group/src/component/btn-group.tsx
+++ lib/btn-group/src/component/btn-group.tsx
@@ -1,36 +1,31 @@
 import {$} from '@zui/core';
 import {Button} from '@zui/button/src/component';
 import {List} from '@zui/list/src/component';
-import {DropdownButton} from '@zui/dropdown/src/component/dropdown-button';
+import {DropdownButton} from '@zui/dropdown/src/component';
 
 import type {RenderableProps} from 'preact';
 import type {ClassNameLike} from '@zui/core';
+import type {ListProps, Item} from '@zui/list';
 import type {BtnGroupOptions} from '../types';
+
+function getBtnProps(item: Item, props: ListProps) {
+    const {style, rootClass, class: className, class: className2, ...others} = item;
+    const {btnProps = {}, btnType} = props as BtnGroupOptions;
+    return {
+        btnType,
+        ...btnProps,
+        ...others,
+        style: btnProps.style ? $.extend({}, btnProps.style, style) : style,
+        className: [className, className2, rootClass, btnProps.className],
+        type: '',
+    };
+}
 
 export class BtnGroup<T extends BtnGroupOptions = BtnGroupOptions> extends List<T> {
     static ItemComponents: typeof List.ItemComponents = {
         ...List.ItemComponents,
-        item: [Button, (item, props) => {
-            const {style, ...others} = item;
-            const {btnProps = {}, btnType} = props as BtnGroupOptions;
-            return {
-                btnType,
-                ...btnProps,
-                ...others,
-                style: btnProps.style ? $.extend({}, btnProps.style, style) : style,
-                type: '',
-            };
-        }],
-        dropdown: [DropdownButton, (item, props) => {
-            const {size, btnProps = {}, btnType} = props as BtnGroupOptions;
-            return {
-                size,
-                btnType,
-                ...btnProps,
-                ...item,
-                type: '',
-            };
-        }],
+        item: [Button, getBtnProps],
+        dropdown: [DropdownButton, getBtnProps],
     };
 
     static NAME = 'btn-group';
