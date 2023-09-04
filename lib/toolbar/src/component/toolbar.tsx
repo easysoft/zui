e8db5e203533e4d730b0c7312b387a3ffbfec161
sunhao <sunhao@easycorp.ltd>
2023-09-04 16:19:57 +0800

    * toolbar: refactor.

--- lib/toolbar/src/component/toolbar.tsx
+++ lib/toolbar/src/component/toolbar.tsx
@@ -4,10 +4,6 @@ import type {Item} from '@zui/list';
 import type {ToolbarOptions} from '../types';
 
 export class Toolbar<T extends ToolbarOptions = ToolbarOptions> extends BtnGroup<T> {
-    static ItemComponents: typeof BtnGroup.ItemComponents = {
-        ...BtnGroup.ItemComponents,
-    };
-
     static NAME = 'toolbar';
 
     static defaultItemProps: Partial<Item> = {
