7c1a37a100f4c060574d2db44f1768a37fea1824
sunhao <sunhao@easycorp.ltd>
2023-09-07 18:09:19 +0800

    * core: add removeUndefinedProps helper.

--- lib/core/src/helpers/merge-props.ts
+++ lib/core/src/helpers/merge-props.ts
@@ -1,12 +1,12 @@
 import {$} from '../cash';
 import {ClassNameLike, classes} from './classes';
 
-export function mergeProps(props: Record<string, unknown>, ...args: unknown[]) {
+export function mergeProps<T extends Record<string, unknown> = Record<string, unknown>>(props: Record<string, unknown>, ...args: unknown[]) {
     args.forEach(arg => {
         if (!$.isPlainObject(arg)) {
             return;
         }
-        Object.keys(arg).forEach(key => {
+        Object.keys(arg as Partial<T>).forEach(key => {
             let value = arg[key];
             if (key === 'className' || key.endsWith('Class')) {
                 value = props[key] ? classes(props[key] as ClassNameLike, value) : value;
@@ -15,6 +15,15 @@ export function mergeProps(props: Record<string, unknown>, ...args: unknown[]) {
             }
             props[key] = value;
         });
+    });
+    return props;
+}
+
+export function removeUndefinedProps(props: Record<string, unknown>) {
+    Object.keys(props).forEach(key => {
+        if (props[key] === undefined) {
+            delete props[key];
+        }
     });
     return props;
 }
