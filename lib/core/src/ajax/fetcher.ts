418fc57d66cbb06914f6abafb1f350a88f624a3c
sunhao <sunhao@easycorp.ltd>
2023-09-07 12:01:19 +0800

    * core: set default dataType to json on fetch data.

--- lib/core/src/ajax/fetcher.ts
+++ lib/core/src/ajax/fetcher.ts
@@ -3,7 +3,7 @@ import {Ajax} from './ajax';
 import type {AjaxSetting, FetcherSetting} from './types';
 
 export async function fetchData<T = {}, A extends unknown[] = unknown[]>(setting: FetcherSetting<T, A>, args: A = ([] as unknown as A), extraAjaxSetting?: Partial<AjaxSetting> | ((ajaxSetting: AjaxSetting) => Partial<AjaxSetting>)): Promise<T> {
-    const ajaxSetting = {throws: true} as AjaxSetting;
+    const ajaxSetting = {throws: true, dataType: 'json'} as AjaxSetting;
     if (typeof setting === 'string') {
         ajaxSetting.url = setting;
     } else if (typeof setting === 'object') {
