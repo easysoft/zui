bf0cdedeccfe8ce3afd496f88670ab759d1584fb
sunhao <sunhao@easycorp.ltd>
2023-09-10 18:40:02 +0800

    * nav: refactor with the new CommonList component.

--- lib/nav/src/component/nav.tsx
+++ lib/nav/src/component/nav.tsx
@@ -1,20 +1,16 @@
-import {List, ListItem} from '@zui/list/src/component';
+import {List} from '@zui/list/src/component';
 
+import type {Item} from '@zui/common-list';
 import type {RenderableProps} from 'preact';
 import type {ClassNameLike} from '@zui/core';
-import type {Item} from '@zui/list';
 import type {NavOptions} from '../types';
 
 export class Nav<T extends NavOptions = NavOptions> extends List<T> {
     static NAME = 'nav';
 
-    static ItemComponents: typeof List.ItemComponents = {
-        ...List.ItemComponents,
-        item: [ListItem, {innerComponent: 'a'}],
-    };
-
     static defaultItemProps: Partial<Item> = {
         component: 'li',
+        innerComponent: 'a',
     };
 
     protected _getClassName(props: RenderableProps<T>): ClassNameLike {
