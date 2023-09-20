67d6b978a97dcea1121c970d52bbfd1e449509cf
sunhao <sunhao@easycorp.ltd>
2023-09-20 13:39:30 +0800

    * core: add method to generate css size value.

--- lib/core/src/helpers/size.ts
+++ lib/core/src/helpers/size.ts
@@ -1,16 +1,21 @@
 export type SizeSetting = number | `${number}%` | `${number}px` | `${number}/${number}` | (string & {});
 
-export function parseSize(params: SizeSetting): [value: number, type?: 'px' | '%'] {
-    if (typeof params === 'number') {
-        return [params];
+export function parseSize(size: SizeSetting): [value: number, type?: 'px' | '%'] {
+    if (typeof size === 'number') {
+        return [size];
     }
-    let match = params.match(/(\d+)(%|px)?/);
+    let match = size.match(/(\d+)(%|px)?/);
     if (match) {
         return [parseInt(match[1]), match[2] as 'px' | '%'];
     }
-    match = params.match(/(\d+)\/(\d+)/);
+    match = size.match(/(\d+)\/(\d+)/);
     if (match) {
         return [100 * parseInt(match[1]) / parseInt(match[2]), '%'];
     }
     return [NaN];
+}
+
+export function toCssSize(size: SizeSetting): string {
+    const [val, unit = 'px'] = parseSize(size);
+    return `${val}${unit}`;
 }
