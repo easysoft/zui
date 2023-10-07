aec3f1715bb7fba979fef06af09298866d20c70c
sunhao <sunhao@easycorp.ltd>
2023-10-07 14:24:53 +0800

    * utilities: refactor.

--- lib/utilities/src/main.ts
+++ lib/utilities/src/main.ts
@@ -1,7 +1,6 @@
 import './vars.css';
 import './interactivity/index.css';
 import './layout/index.css';
-import './flex/index.css';
 import './spacing/index.css';
 import './sizing/index.css';
 import './style/index.css';
@@ -9,11 +8,4 @@ import './typography/index.css';
 import './backgrounds/index.css';
 import './borders/index.css';
 import './effects/index.css';
-import {$} from '@zui/core';
-
-$(() => {
-    $('.disabled, [disabled]').on('click', (e) => {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-    });
-});
+import './flex/index.css';
