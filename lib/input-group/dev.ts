28d2662f88fc248d10cf178cb67fda6f135f804c
sunhao <sunhao@easycorp.ltd>
2023-11-27 14:26:20 +0800

    * input-group: refactor style.

--- lib/input-group/dev.ts
+++ lib/input-group/dev.ts
@@ -1,5 +1,7 @@
-import '@zui/dropdown';
+import '@zui/list';
+import '@zui/menu';
 import '@zui/icons';
 import '@zui/checkbox';
 import '@zui/button';
+import '@zui/dropdown';
 import './src/main';
