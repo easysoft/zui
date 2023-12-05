d5f2c09dfcef9780b3e94b22e37fd930e5b7f24f
sunhao <sunhao@easycorp.ltd>
2023-12-05 12:31:02 +0800

    * core: add helper method to get hotkeys binding map.

--- lib/core/src/helpers/hotkeys.ts
+++ lib/core/src/helpers/hotkeys.ts
@@ -11,11 +11,64 @@ export type HotkeysBindingMap = KeyBindingMap;
 
 export type HotkeyBindingCallback = (event: KeyboardEvent) => void;
 
+export type HotkeySetting = {
+    keys: string | string[];
+    handler: HotkeyBindingCallback;
+    optional?: boolean;
+};
+
+export type HotkeyName = string;
+
+export type HotkeysMap = Record<HotkeyName, HotkeySetting>;
+
+export type HotkeysSettings = boolean | Record<HotkeyName, HotkeySetting | boolean>;
+
+export function getHotkeysMap(settings: HotkeysSettings, defaultMap: HotkeysMap = {}): HotkeysBindingMap | undefined {
+    if (!settings) {
+        return;
+    }
+
+    const map = Object.keys(defaultMap).reduce<HotkeysMap>((currentMap, name) => {
+        if (!defaultMap[name].optional) {
+            currentMap[name] = {
+                ...defaultMap[name],
+            };
+        }
+        return currentMap;
+    }, {});
+    Object.keys(settings).forEach((name) => {
+        const setting = (settings as Record<HotkeyName, HotkeySetting | boolean>)[name];
+        if (!setting) {
+            delete map[name];
+        } else if (setting === true) {
+            if (defaultMap[name]) {
+                map[name] = {
+                    ...defaultMap[name],
+                };
+            }
+        } else {
+            map[name] = setting;
+        }
+    });
+
+    return Object.keys(map).reduce<HotkeysBindingMap>((bindingMap, name) => {
+        const {keys, handler} = map[name];
+        if (typeof keys === 'string') {
+            bindingMap[keys] = handler;
+        } else {
+            keys.forEach((key) => {
+                bindingMap[key] = handler;
+            });
+        }
+        return bindingMap;
+    }, {});
+}
+
 export function bindHotkeys(selector: Selector, bindingMap: HotkeysBindingMap, options?: HotkeysBindingOptions) {
     const {timeout, event = 'keydown', scope, when} = options || {};
     const handler = createKeybindingsHandler(bindingMap, {timeout});
     return $(selector).on(`${event}.zui.hotkeys${scope ? `.${scope}` : ''}`, function (e: KeyboardEvent) {
-        if (!when || when(e) === false) {
+        if (when && when(e) === false) {
             return;
         }
         handler(e);
