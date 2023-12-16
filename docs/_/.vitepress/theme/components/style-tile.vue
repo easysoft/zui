1ea7005e5a30a1d1cb09975f97a6871fe818bd4d
sunhao <sunhao@easycorp.ltd>
2023-12-16 16:47:56 +0800

    * docs: add more props to StyleTile component.

--- docs/_/.vitepress/theme/components/style-tile.vue
+++ docs/_/.vitepress/theme/components/style-tile.vue
@@ -1,27 +1,57 @@
 <template>
   <div class="style-tile-item" @click="onColorClick">
-    <div class="style-tile" :class="[name, tileClass, copied ? ' ring-4 ring-opacity-50' : '']">
-      {{ title === true ? name : (title ?? '') }}
+    <div class="style-tile" :class="[name, tileClass, copied ? 'ring-4 ring-opacity-50' : '']">
+      {{ titleText }}
       <slot />
     </div>
+    <div v-if="labelText" class="style-tile-label" :class="labelClass">{{ labelText }}</div>
     <div v-if="copied" class="right-0 text-center style-tile-name is-copied success">已复制</div>
-    <div v-else class="pr-1 style-tile-name bg-canvas">{{hint ?? `${name}${alias ? ` 别名: ${alias}` : ''}`}}</div>
+    <div v-else-if="hintText" class="pr-1 style-tile-name bg-canvas">{{hintText}}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref, onUnmounted} from 'vue';
+import {ref, computed, onUnmounted} from 'vue';
 
 const props = defineProps<{
   name: string;
   title?: string | boolean;
   tileClass?: string;
+  labelClass?: string;
   alias?: string;
-  hint?: string;
+  hint?: string | false;
+  label?: string | boolean;
 }>();
 
 const copied = ref(false);
 const tipTimer = ref(0);
+
+const titleText = computed(() => {
+  const {title} = props;
+  if (!title) {
+    return '';
+  }
+  return title === true ? props.name : title;
+});
+
+const labelText = computed(() => {
+  const {label} = props;
+  if (!label) {
+    return '';
+  }
+  return label === true ? props.name : label;
+});
+
+const hintText = computed(() => {
+  const {hint} = props;
+  if (hint === false) {
+    return '';
+  }
+  if (typeof hint === 'string') {
+    return hint;
+  }
+  return `${props.name}${props.alias ? ` 别名: ${props.alias}` : ''}`;
+});
 
 const onColorClick = () => {
   navigator.clipboard.writeText(props.name);
@@ -49,8 +79,14 @@ onUnmounted(() => {
 .style-tile-item:hover > .style-tile {
   @apply -scale-105 -shadow-lg;
 }
+.style-tile-label {
+  @apply -text-sm -mt-1 -opacity-80;
+}
 .style-tile-name {
   @apply -absolute -left-0 -mt-0.5 -top-full -opacity-0 -font-mono -text-xs -bg-opacity-50 -backdrop-blur -whitespace-nowrap -rounded -scale-75 -transition-all -z-10 -p-0.5;
+}
+.style-tile-label + .style-tile-name {
+  @apply --mt-4;
 }
 .style-tile-item:hover > .style-tile-name {
   @apply -scale-100 -opacity-100 -delay-300;
