de25e22357ff9c25b20e3e9cb088548aeeeb37da
sunhao <sunhao@easycorp.ltd>
2023-12-17 15:36:44 +0800

    * docs: support to format output with in CssPropValue component.

--- docs/_/.vitepress/theme/components/css-prop-value.vue
+++ docs/_/.vitepress/theme/components/css-prop-value.vue
@@ -1,18 +1,29 @@
 <template>
-  <span ref="ele">{{value}}</span>
+  <span ref="ele">{{display}}</span>
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
+import {onMounted, ref, computed} from 'vue';
 
 const props = defineProps<{
   prop: string,
   placeholder?: string,
   target?: string,
+  format?: string | ((value: string) => string),
 }>();
 
 const value = ref(props.placeholder ?? props.prop);
 const ele = ref();
+const display = computed(() => {
+  const {format} = props;
+  if (typeof format === 'function') {
+    return format(value.value);
+  }
+  if (typeof format === 'string') {
+    return format.replaceAll('{0}', value.value);
+  }
+  return value.value;
+});
 
 onMounted(() => {
   const updateValue = () => {
