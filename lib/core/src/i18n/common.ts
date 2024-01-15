0b3b732c5909ecba350ea0cfa67553dc69155c6a
sunhao <sunhao@easycorp.ltd>
2024-01-15 09:01:32 +0800

    * core: add more common lang data.

--- lib/core/src/i18n/common.ts
+++ lib/core/src/i18n/common.ts
@@ -3,20 +3,26 @@ import {addI18nMap} from './i18n';
 addI18nMap({
     'zh_cn': {
         confirm: '确定',
+        save: '保存',
         cancel: '取消',
         delete: '删除',
+        reset: '重置',
         add: '添加',
     },
     'zh_tw': {
         confirm: '確定',
+        save: '儲存',
         cancel: '取消',
         delete: '刪除',
+        reset: '重置',
         add: '添加',
     },
     en: {
         confirm: 'Confirm',
+        save: 'Save',
         cancel: 'Cancel',
         delete: 'Delete',
+        reset: 'Reset',
         add: 'Add',
     },
 });
