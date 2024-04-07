218f1a849eba5160cca8c37d0fa0221401dc4de0
sunhao <sunhao@easycorp.ltd>
2024-04-07 11:40:40 +0800

    * core: refactor type.

--- lib/core/src/config/index.ts
+++ lib/core/src/config/index.ts
@@ -3,7 +3,7 @@
 declare const __APP_VERSION__: string;
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-declare const __BUILD_TIME__: string;
+declare const __BUILD_TIME__: number;
 
 export const VERSION = __APP_VERSION__;
 export const BUILD = __BUILD_TIME__;
