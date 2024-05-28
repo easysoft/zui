be5a4779c11504b056200ea98890ba6e17ea86f3
sunhao <sunhao@easycorp.ltd>
2024-05-28 22:51:56 +0800

    + sticky: support for using sticky in scrollable container.

--- lib/core/src/helpers/sticky.ts
+++ lib/core/src/helpers/sticky.ts
@@ -5,7 +5,8 @@ export type StickyOptions = {
     offset?: number;
     zIndex?: number;
     pinnedClass?: string;
-    watch?: string;
+    targets?: string;
+    scrollContainer?: string;
 };
 
 export class Sticky extends Component<StickyOptions> {
@@ -13,20 +14,61 @@ export class Sticky extends Component<StickyOptions> {
 
     protected declare _ob: IntersectionObserver;
 
+    protected declare _container: HTMLElement;
+
+    protected declare _scrollListener: () => void;
+
+    protected declare _raf: number;
+
     init() {
-        const {offset = 1, side, zIndex, pinnedClass = 'is-pinned', watch} = this.options;
+        const {offset = 1, side, zIndex, pinnedClass = 'is-pinned', targets, scrollContainer} = this.options;
         const {$element} = this;
-        const $watch = watch ? $element.find(watch) : $element;
-        $watch.css({position: 'sticky', zIndex});
-        if (side) $watch.css(side, -offset);
-        this._ob = new IntersectionObserver(
-            ([e]) => e.target.classList.toggle(pinnedClass, e.intersectionRatio < offset),
-            {threshold: [1]},
-        );
-        $watch.each((_, e) => this._ob.observe(e));
+        const $targets = targets ? $element.find(targets) : $element;
+        $targets.css({position: 'sticky', zIndex});
+        if (side) $targets.css(side, -offset);
+
+        if (scrollContainer) {
+            const container = $element.closest(scrollContainer)[0] as HTMLElement;
+            if (container) {
+                const listener = () => {
+                    if (this._raf) {
+                        cancelAnimationFrame(this._raf);
+                    }
+                    this._raf = requestAnimationFrame(() => {
+                        const containerRect = container.getBoundingClientRect();
+                        $targets.each((_, target) => {
+                            const rect = target.getBoundingClientRect();
+                            const pinned = rect.top === containerRect.top;
+                            target.classList.toggle(pinnedClass, pinned);
+                        });
+                        this._raf = 0;
+                    });
+                };
+                this._scrollListener = listener;
+                container.addEventListener('scroll', listener);
+            }
+            this._container = container;
+        } else {
+            this._ob = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach(e => {
+                        e.target.classList.toggle(pinnedClass, e.intersectionRatio < offset);
+                    });
+                },
+                {threshold: [1]},
+            );
+
+            $targets.each((_, e) => this._ob.observe(e));
+        }
     }
 
     destroy() {
-        this._ob.disconnect();
+        this._ob?.disconnect();
+        if (this._container) {
+            this._container.removeEventListener('scroll', this._scrollListener);
+            if (this._raf) {
+                cancelAnimationFrame(this._raf);
+            }
+        }
     }
 }
