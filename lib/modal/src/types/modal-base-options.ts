37da5e0b3798269d5d4c2ca089a065ca2a1b3b71
sunhao <sunhao@easycorp.ltd>
2024-09-12 08:51:17 +0800

    * modal: refactor.

--- lib/modal/src/types/modal-base-options.ts
+++ lib/modal/src/types/modal-base-options.ts
@@ -1,8 +1,8 @@
-import {JSX} from 'preact';
-import {ClassNameLike} from '@zui/core';
-import {ModalBackdropType} from './modal-backdrop-type';
-import {ModalPositionSetting} from './modal-position-setting';
-import {ModalSizeSetting} from './modal-size-setting';
+import type {JSX} from 'preact';
+import type {ClassNameLike} from '@zui/core';
+import type {ModalBackdropType} from './modal-backdrop-type';
+import type {ModalPositionSetting} from './modal-position-setting';
+import type {ModalSizeSetting} from './modal-size-setting';
 import type {ModalBase} from '../vanilla';
 
 export type ModalBaseOptions = {
