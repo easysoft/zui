9ef7abfc30585ee6a52e000ebcbf5ff7748f9028
sunhao <sunhao@easycorp.ltd>
2024-09-12 08:50:02 +0800

    * core: fix typo.

--- lib/core/src/react/components/h-element-signals.ts
+++ lib/core/src/react/components/h-element-signals.ts
@@ -6,12 +6,12 @@ import type {HElementProps} from '../types';
 export class HElementSignals<P extends HElementProps, S = {}, SIGNALS = {readonly [K in keyof S]-?: Signal<S[K]>}> extends HElement<P, S> {
     static HElementSignals = true;
 
-    protected declare singals: SIGNALS;
+    protected declare signals: SIGNALS;
 
     constructor(props: P) {
         super(props);
 
-        this.singals = {} as SIGNALS;
+        this.signals = {} as SIGNALS;
         const {state} = this;
         this.changeState(state);
         this.state = {} as S;
@@ -23,11 +23,11 @@ export class HElementSignals<P extends HElementProps, S = {}, SIGNALS = {readonl
                 state = state(this.state);
             }
             for (const key in state) {
-                const sg = this.singals[key as unknown as keyof SIGNALS] as Signal;
+                const sg = this.signals[key as unknown as keyof SIGNALS] as Signal;
                 if (sg) {
                     sg.value = state[key as keyof S];
                 } else {
-                    this.singals[key as unknown as keyof SIGNALS] = signal(state[key as keyof S]) as SIGNALS[keyof SIGNALS];
+                    this.signals[key as unknown as keyof SIGNALS] = signal(state[key as keyof S]) as SIGNALS[keyof SIGNALS];
                 }
             }
             callback?.();
