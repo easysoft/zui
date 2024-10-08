9be163d1af9fc420b7cdea8253c6a30d2d648df0
sunhao <sunhao@easycorp.ltd>
2024-10-08 15:53:20 +0800

    * popover: set command proxy to popover element.

--- lib/popover/src/vanilla/popover.ts
+++ lib/popover/src/vanilla/popover.ts
@@ -203,7 +203,7 @@ export class Popover<O extends PopoverOptions = PopoverOptions, E extends Compon
         this._zIndex = Popover.Z_INDEX++;
         this._shown = true;
         this.render();
-        SHOWN_POPOVERS.set(this.gid, this);
+        SHOWN_POPOVERS.set(this.gid, this as Popover);
         onShow?.call(this);
         this.emit('show');
 
@@ -229,8 +229,11 @@ export class Popover<O extends PopoverOptions = PopoverOptions, E extends Compon
                 });
         }
 
-        if (!this._virtual && elementShowClass) {
-            $(this._triggerElement as HTMLElement).addClass(elementShowClass);
+        if (!this._virtual) {
+            $target.attr('zui-command-proxy', '').data('zui.commandProxy', this._triggerElement);
+            if (elementShowClass) {
+                $(this._triggerElement as HTMLElement).addClass(elementShowClass);
+            }
         }
 
         this._resetTimer(() => {
@@ -262,8 +265,11 @@ export class Popover<O extends PopoverOptions = PopoverOptions, E extends Compon
             $target.off(this.namespace);
         }
 
-        if (!this._virtual && elementShowClass) {
-            $(this._triggerElement as HTMLElement).removeClass(elementShowClass).removeAttr('data-pop-placement');
+        if (!this._virtual) {
+            $target.removeAttr('zui-command-proxy').removeData('zui.commandProxy');
+            if (elementShowClass) {
+                $target.removeClass(elementShowClass).removeAttr('data-pop-placement');
+            }
         }
 
         if (hideNewOnHide) {
