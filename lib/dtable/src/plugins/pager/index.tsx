52bdf77d4231bc6f14e1684b813bd9a76476b77a
sunhao <sunhao@easycorp.ltd>
2024-10-16 10:29:34 +0800

    * dtable: support for using pager as local pager to filter data rows.

--- lib/dtable/src/plugins/pager/index.tsx
+++ lib/dtable/src/plugins/pager/index.tsx
@@ -1,4 +1,4 @@
-import {PagerOptions} from '@zui/pager/src/types';
+import type {PagerInfo, PagerOptions} from '@zui/pager';
 import {Pager} from '@zui/pager/src/component';
 import {definePlugin} from '../../helpers/shared-plugins';
 
@@ -7,17 +7,70 @@ import type {DTablePlugin, DTableWithPlugin} from '../../types/plugin';
 export interface DTablePagerTypes {
     options: Partial<{
         footPager: PagerOptions,
+        localPager: PagerInfo | boolean,
     }>,
+    state: {
+        pager?: PagerInfo;
+    },
 }
 
 export type DTablePager = DTableWithPlugin<DTablePagerTypes>;
 
 const pagerPlugin: DTablePlugin<DTablePagerTypes> = {
     name: 'pager',
+    state() {
+        const localPager = this.props.localPager as DTablePagerTypes['options']['localPager'];
+        if (localPager) {
+            const {page = 1, recTotal = 0, recPerPage = 20, pageTotal = 1} = (this.props.footPager || {}) as Partial<PagerInfo>;
+            return {
+                pager: {
+                    page,
+                    recTotal,
+                    recPerPage,
+                    pageTotal,
+                    ...(typeof localPager === 'object' ? localPager : null),
+                },
+            };
+        }
+        return {};
+    },
     footer: {
         pager() {
-            const {footPager} = this.options;
+            let {footPager} = this.options;
+            const {localPager} = this.options;
             if (footPager) {
+                footPager = {
+                    items: [
+                        {
+                            'type': 'link',
+                            'page': 'first',
+                            'icon': 'icon-first-page',
+                        },
+                        {
+                            'type': 'link',
+                            'page': 'prev',
+                            'icon': 'icon-angle-left',
+                        },
+                        {
+                            'type': 'info',
+                            'text': '{page}/{pageTotal}',
+                        },
+                        {
+                            'type': 'link',
+                            'page': 'next',
+                            'icon': 'icon-angle-right',
+                        },
+                        {
+                            'type': 'link',
+                            'page': 'last',
+                            'icon': 'icon-last-page',
+                        },
+                    ],
+                    ...footPager,
+                    ...(typeof localPager === 'object' ? localPager : null),
+                    ...this.state.pager,
+                    recTotal: this.layout.allRows.length,
+                };
                 if (Array.isArray(footPager.items)) {
                     footPager.items.forEach(item => {
                         if (item.type === 'size-menu' && item.caret === undefined) {
@@ -25,10 +78,42 @@ const pagerPlugin: DTablePlugin<DTablePagerTypes> = {
                         }
                     });
                 }
+                if (this.options.localPager) {
+                    footPager.onChangePageSize = (info) => {
+                        this.update({
+                            dirtyType: 'layout',
+                            state: (prevState) => {
+                                const pager = {...(prevState as unknown as DTablePagerTypes['state']).pager, recPerPage: info.recPerPage};
+                                return {pager};
+                            },
+                        });
+                    };
+                    footPager.onGoToPage = (info) => {
+                        this.update({
+                            dirtyType: 'layout',
+                            state: (prevState) => {
+                                const pager = {...(prevState as unknown as DTablePagerTypes['state']).pager, page: info.page};
+                                return {pager};
+                            },
+                        });
+                    };
+                }
                 return [<Pager {...footPager} />];
             }
             return [];
         },
+    },
+    onAddRows(rows) {
+        const {localPager} = this.options;
+        if (localPager) {
+            const {page = 1, recPerPage = 20} = {
+                ...(typeof localPager === 'object' ? localPager : null),
+                ...this.state.pager,
+            };
+            const start = (page - 1) * recPerPage;
+            const end = Math.min(page * recPerPage, rows.length);
+            return rows.slice(start, end);
+        }
     },
 };
 
