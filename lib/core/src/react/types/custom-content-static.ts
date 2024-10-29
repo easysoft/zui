89511e7693793033b2d2b5b533cc30707d7ffc01
sunhao <sunhao@easycorp.ltd>
2024-10-29 10:06:10 +0800

    * core: support for using lazy content in CustomContent.

--- lib/core/src/react/types/custom-content-static.ts
+++ lib/core/src/react/types/custom-content-static.ts
@@ -1,5 +1,6 @@
 import type {ComponentChildren} from 'preact';
 import type {HtmlContentProps} from './html-content-props';
 import type {HElementProps} from './h-element-props';
+import type {LazyContentProps} from './lazy-content-props';
 
-export type CustomContentStatic = ComponentChildren | HtmlContentProps | HElementProps;
+export type CustomContentStatic = ComponentChildren | HtmlContentProps | LazyContentProps | HElementProps;
