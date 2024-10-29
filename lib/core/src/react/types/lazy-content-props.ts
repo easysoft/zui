5a99de27ea5360f5eb6404e451ff18e85555309f
sunhao <sunhao@easycorp.ltd>
2024-10-29 11:14:30 +0800

    * core: refactor types.

--- lib/core/src/react/types/lazy-content-props.ts
+++ lib/core/src/react/types/lazy-content-props.ts
@@ -1,10 +1,10 @@
 import type {FetcherSetting} from '../../ajax';
 import type {CustomContentType} from './custom-content-type';
 
-export type LazyContentProps = {
-    fetcher: FetcherSetting<string | CustomContentType>;
-    fetcherArgs: unknown[];
-    fetcherThis?: unknown;
+export type LazyContentProps<T = string | CustomContentType, A extends unknown[] = unknown[], THIS = unknown> = {
+    fetcher: FetcherSetting<T, A, THIS>;
+    fetcherArgs: A;
+    fetcherThis?: THIS;
     loadingText?: string;
     errorText?: string;
     type?: 'html' | 'text' | 'custom';
