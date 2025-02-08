c896b9e37371932ea3b0340366182ac6fb698cd2
sunhao <sunhao@easycorp.ltd>
2025-02-08 17:05:25 +0800

    * core: change special types.

--- lib/core/src/types/specials.ts
+++ lib/core/src/types/specials.ts
@@ -2,4 +2,6 @@ export type NullValue = null | undefined;
 
 export type EmptyValue = null | undefined | '';
 
-export type FalseValue = null | undefined | '' | false;
+export type FalsyValue = null | undefined | '' | false | 0 | -0 | typeof NaN | 0n;
+
+export type NullableString = string | null | undefined;
