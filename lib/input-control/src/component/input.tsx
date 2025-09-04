14e3e86782532188b281c2cce3a7d6dc9f7eaaff
catouse <catouse@gmail.com>
2025-09-04 16:09:53 +0800

    * input-control: enhance Input component with additional props and update state management for controlled input behavior.

--- lib/input-control/src/component/input.tsx
+++ lib/input-control/src/component/input.tsx
@@ -1,10 +1,13 @@
-import {Component} from 'preact';
+import {Component, JSX} from 'preact';
 import type {JSXInternal} from 'preact/src/jsx';
 import {classes} from '@zui/core';
 
 type InputProps = {
+    className?: string;
+    style?: JSX.CSSProperties;
     type?: string;
     defaultValue?: string;
+    value?: string;
     onChange?: (value: string) => void;
     icon?: string;
 };
@@ -41,16 +44,22 @@ export default class Input extends Component<InputProps, InputState> {
         }
     };
 
+    componentDidUpdate(previousProps: Readonly<InputProps>): void {
+        if (this.props.value !== undefined && previousProps.value !== this.props.value) {
+            this.setState({value: this.props.value});
+        }
+    }
+
     render() {
-        const {type = 'text', icon} = this.props;
+        const {type = 'text', icon, value: _value, defaultValue, className, style, onChange, ...rest} = this.props;
         const {value} = this.state;
 
         const iconView = icon ? <label className="input-control-prefix"><i className={`icon icon-${icon}`}></i></label> : null;
 
         return (
-            <div className="zui-input input-control has-prefix-icon">
+            <div className={classes('zui-input input-control has-prefix-icon', className)} style={style}>
                 {iconView}
-                <input className="form-control" type={type} value={value} onChange={this.handleChange} />
+                <input className="form-control" type={type} value={value} onChange={this.handleChange} {...rest} />
                 <span className={classes('-absolute -w-8 -h-8 -right-0 -top-0 -flex -justify-center -items-center -cursor-pointer', {'-hidden': !value})} onClick={this.handleClear}>
                     <i className="icon icon-close"></i>
                 </span>
