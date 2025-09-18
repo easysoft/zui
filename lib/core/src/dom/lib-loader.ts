9437dc376641011da70e27c663874437af9a4d78
catouse <catouse@gmail.com>
2025-09-18 11:16:10 +0800

    * core: introduce LibLoader class for dynamic library loading and registration, enhancing modularity and error handling.

---
+++ lib/core/src/dom/lib-loader.ts
@@ -0,0 +1,30 @@
+import {getLib, registerLib, type GetLibOptions} from './get-lib';
+
+export class LibLoader<T = unknown> {
+    protected _module?: T;
+
+    protected _name: string;
+
+    constructor(name: string, options?: GetLibOptions) {
+        this._name = name;
+        if (options) {
+            this.register(options);
+        }
+    }
+
+    get Module(): T {
+        if (!this._module) {
+            throw new Error('[ZUI] LibLoader.Module is not loaded.');
+        }
+        return this._module;
+    }
+
+    register(options: GetLibOptions) {
+        registerLib(this._name, options);
+    }
+
+    async load() {
+        this._module = await getLib(this._name);
+        return this._module;
+    }
+}
