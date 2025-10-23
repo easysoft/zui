73bd1982d5a2f2e101c368da5402d5875f9af442
catouse <catouse@gmail.com>
2025-10-23 14:22:48 +0800

    * string-helper: add escapeHtml function to sanitize HTML input for improved security and prevent XSS vulnerabilities.

--- lib/helpers/src/string-helper.ts
+++ lib/helpers/src/string-helper.ts
@@ -107,3 +107,11 @@ export const convertBytes = (str: string) => {
     str = str.replace(unit, '');
     return Number.parseInt(str, 10) * BYTE_UNITS[unit];
 };
+
+export const excapeHtml = (html: string) => {
+    return html.replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+};
