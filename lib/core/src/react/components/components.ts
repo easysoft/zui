500b897977abadf31c1a1e2d5e60f54272c7851e
catouse <catouse@gmail.com>
2025-11-11 11:32:40 +0800

    * core: introduce reactComponentMap for improved component registration and retrieval.

--- lib/core/src/react/components/components.ts
+++ lib/core/src/react/components/components.ts
@@ -1,5 +1,7 @@
 import type {ComponentType} from 'preact';
 
+export const reactComponentMap = new Map<string, ComponentType>();
+
 export const reactComponents: Record<string, ComponentType> = {};
 
 export function registerReactComponent<P = object>(name: string, component?: ComponentType<P>): void;
@@ -12,10 +14,11 @@ export function registerReactComponent(nameOrMap: string | Record<string, unknow
             registerReactComponent(name, nameOrMap[name] as ComponentType);
         });
     } else if (component) {
-        reactComponents[nameOrMap.toLowerCase()] = component;
+        reactComponents[nameOrMap] = component;
+        reactComponentMap.set(nameOrMap.toLowerCase(), component);
     }
 }
 
 export function getReactComponent<P = object>(name: string): ComponentType<P> | undefined {
-    return reactComponents[name.toLowerCase()] as (ComponentType<P> | undefined);
+    return reactComponentMap.get(name.toLowerCase()) as (ComponentType<P> | undefined);
 }
