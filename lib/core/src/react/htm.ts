9fe7e3c6cbba642815501ca8900fe28ec5b47bf8
catouse <catouse@gmail.com>
2025-12-03 13:48:00 +0800

    * core: update pnpm lockfile version and dependencies, including vhtml integration in core package, enhancing rendering capabilities and ensuring compatibility with updated libraries.

--- lib/core/src/react/htm.ts
+++ lib/core/src/react/htm.ts
@@ -1,10 +1,13 @@
 import htm from 'htm';
+import vhtml from 'vhtml';
 import {h} from './preact';
 
-const html = htm.bind(h);
+const jsx = htm.bind(h);
+const html = htm.bind(vhtml);
 
 export {
     htm,
     html,
-    html as jsx,
+    jsx,
+    vhtml,
 };
