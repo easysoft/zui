9419c343e5e875ea2b5d4fc27f1b91e5984e9040
catouse <catouse@gmail.com>
2026-01-29 14:10:36 +0800

    * nav: extend Nav component to include dropdown item support with enhanced rendering logic.

--- lib/nav/src/component/nav.tsx
+++ lib/nav/src/component/nav.tsx
@@ -1,12 +1,25 @@
 import {mergeProps, type ClassNameLike} from '@zui/core';
 import {List} from '@zui/list/src/component/list';
+import {DropdownButton} from '@zui/dropdown/src/component';
 
 import type {Item} from '@zui/common-list';
 import type {Attributes, RenderableProps} from 'preact';
 import type {NavOptions, NavSetting} from '../types';
+import {DropdownButtonOptions} from '@zui/dropdown/src/types';
 
 export class Nav<T extends NavOptions = NavOptions> extends List<T> {
     static NAME = 'nav';
+
+    static ItemComponents = {
+        ...List.ItemComponents,
+        dropdown: [List.ItemComponents.default, (item: Item) => {
+            const {text, items, menu, props, className, innerClass} = item as Item & DropdownButtonOptions & {props: DropdownButtonOptions};
+            return {
+                className,
+                children: <DropdownButton className={innerClass as string} type="ghost" text={text} items={items} menu={menu} {...props} />,
+            };
+        }],
+    } as unknown as typeof List.ItemComponents;
 
     static defaultItemProps: Partial<Item> = {
         component: 'li',
