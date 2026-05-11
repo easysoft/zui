30af6a960d40ddb883d719167aeb000f288ad288
catouse <catouse@gmail.com>
2026-05-11 11:54:08 +0800

    * core: extract visibility check logic into isViewVisible function for improved readability and reusability

--- lib/core/src/dom/is-visible.ts
+++ lib/core/src/dom/is-visible.ts
@@ -11,6 +11,24 @@ type ISVisibleOptions = {
     checkZeroSize?: boolean;
 };
 
+export function isViewVisible(react: {left: number; top: number; width: number; height: number} | DOMRectReadOnly, viewport: {left: number; top: number; width: number; height: number} | DOMRectReadOnly, fullyCheck?: boolean): boolean {
+    const {left, top, width, height} = react;
+    const {left: viewportLeft, top: viewportTop, width: viewportWidth, height: viewportHeight} = viewport;
+    if (fullyCheck) {
+        return (
+            (left >= viewportLeft)
+            && (top >= viewportTop)
+            && ((left + width) <= (viewportWidth + viewportLeft))
+            && ((top + height) <= (viewportHeight + viewportTop))
+        );
+    }
+    // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+    const horInView = (left <= (viewportLeft + viewportWidth)) && ((left + width) >= viewportLeft);
+    const vertInView = (top <= (viewportTop + viewportHeight)) && ((top + height) >= viewportTop);
+
+    return vertInView && horInView;
+}
+
 /**
  * Check an element whethear is visible in the current viewport.
  *
@@ -39,20 +57,7 @@ export function isVisible(selector: Selector, options: ISVisibleOptions = {}): b
             viewport = {left: 0, top: 0, width: innerWidth || clientWidth, height: innerHeight || clientHeight};
         }
     }
-    const {left: viewportLeft, top: viewportTop, width: viewportWidth, height: viewportHeight} = viewport;
-    if (options.fullyCheck) {
-        return (
-            (left >= viewportLeft)
-            && (top >= viewportTop)
-            && ((left + width) <= (viewportWidth + viewportLeft))
-            && ((top + height) <= (viewportHeight + viewportTop))
-        );
-    }
-    // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
-    const horInView = (left <= (viewportLeft + viewportWidth)) && ((left + width) >= viewportLeft);
-    const vertInView = (top <= (viewportTop + viewportHeight)) && ((top + height) >= viewportTop);
-
-    return vertInView && horInView;
+    return isViewVisible({left, top, width, height}, viewport, options.fullyCheck);
 }
 
 /* Declare types. */
