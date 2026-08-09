e77186baa0f271b92e5701c8a8559b050cf64010
catouse <catouse@gmail.com>
2026-08-09 16:41:19 +0800

    * virtual-grid: fix cell positioning/render precedence and react entry

--- lib/virtual-grid/src/component/virtual-grid.tsx
+++ lib/virtual-grid/src/component/virtual-grid.tsx
@@ -1,17 +1,18 @@
 import {Component} from 'preact';
+import {classes} from '@zui/core';
 import {isCellVisible} from '../helpers/is-cell-visible';
-import {VirtualGridOptions} from '../types/virtual-grid-options';
+import type {VirtualGridOptions} from '../types/virtual-grid-options';
 import {GridCell} from './grid-cell';
 
 export class VirtualGrid extends Component<VirtualGridOptions> {
     render() {
-        const {width, height, cells, left, top, visibleBounding, onRenderCell, style, children, offsetX = 0, offsetY = 0, ...others} = this.props;
+        const {width, height, cells, left, top, visibleBounding, onRenderCell, className, style, children, offsetX = 0, offsetY = 0, ...others} = this.props;
         const visibleCells = visibleBounding ? cells.filter(cell => isCellVisible(cell.bounding, visibleBounding, offsetX, offsetY)) : cells;
 
         return (
-            <div style={{width, height, left, top, ...style}} {...others}>
+            <div className={classes('virtual-grid', className)} style={{position: 'relative', width, height, left, top, ...style}} {...others}>
                 {
-                    visibleCells.map(cell => (<GridCell offsetX={offsetX} offsetY={offsetY} {...cell} />))
+                    visibleCells.map(cell => (<GridCell key={cell.key} offsetX={offsetX} offsetY={offsetY} {...cell} onRender={cell.onRender ?? onRenderCell} />))
                 }
                 {children}
             </div>
